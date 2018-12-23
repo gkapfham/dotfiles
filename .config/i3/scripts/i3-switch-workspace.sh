b4bcc5c823583e6af41150e1485a23825ee48f90
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2018-12-22 23:11:32 -0500

    Use a case insensitive filtering in the switch workspace script.

--- .config/i3/scripts/i3-switch-workspace.sh
+++ .config/i3/scripts/i3-switch-workspace.sh
@@ -5,5 +5,6 @@ function gen_workspaces()
     i3-msg -t get_workspaces | tr ',' '\n' | grep "name" | sed 's/"name":"\(.*\)"/\1/g' | sort -n
 }
 
-WORKSPACE=$( (gen_workspaces) | rofi -width 30 -dmenu -p "workspace:")
+# use rofi to filter workspaces in a case-insensitive fashion
+WORKSPACE=$( (gen_workspaces) | rofi -width 30 -dmenu -i -p "workspace:")
 i3-msg workspace "${WORKSPACE}"
