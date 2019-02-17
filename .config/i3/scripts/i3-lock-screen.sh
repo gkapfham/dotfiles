8e546a6f1b1f53c14e743f45104b2314d259c260
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2019-02-16 19:14:10 -0500

    Add the first version of the i3-lock-screen.sh that runs i3lock (fancy).

---
+++ .config/i3/scripts/i3-lock-screen.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+i3lock --indicator -e --greeter-font='hack' --wrong-font='hack' --verif-font='hack' --greetertext='login' --greeterpos='750:700' --ring-width=15 --greetersize=75 --greetercolor=875f87ff --wrongtext='incorrect' --veriftext='verifying' --locktext='locking' --verifsize=45 --wrongsize=45 --radius=150 --insidecolor=875f87ff --ringvercolor=5f8700ff --keyhlcolor=b5bd68ff --linecolor=875f87ff --ringwrongcolor=5f8700ff --insidevercolor=81a2beff --insidewrongcolor=d75f5fff --line-uses-inside --separatorcolor=5f8700ff -c 1c1c1c
+
+# scrot /tmp/screen.png
+# convert /tmp/screen.png -scale 10% -scale 1000% /tmp/blurred_screen.png
+# # convert /tmp/screen.png -sample 10% -sample 1000% /tmp/blurred_screen.png
+# i3lock -u -i /tmp/blurred_screen.png
+# rm /tmp/screen.png
+# rm /tmp/blurred_screen.png
