9026dcaa1db967137ead2546bf7c110894ec1942
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2020-06-19 23:17:30 -0400

    Improve the polybar-launch.sh script to handle multiple monitors.

--- polybar/scripts/polybar-launch.sh
+++ polybar/scripts/polybar-launch.sh
@@ -6,5 +6,15 @@ killall -q polybar
 # kill confirmed
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-# launch polybar
-polybar mybar &
+# launch polybar for laptop
+# polybar mybar &
+
+# launch polybar for all available monitors reported by xrandr
+# reference: https://github.com/polybar/polybar/issues/763
+if type "xrandr"; then
+  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+    MONITOR=$m polybar --reload mybar &
+  done
+else
+  polybar --reload mybar &
+fi
