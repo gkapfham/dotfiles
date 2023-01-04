b28adc7fbc4858848c537c17657d6c898170e1d3
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2023-01-04 14:53:33 -0500

    Add the template version of the colorscheme.lua and completion.lua.

---
+++ nvim/lua/plugins/colorscheme.lua
@@ -0,0 +1,13 @@
+return {
+
+  -- define the color scheme and load it
+  {
+    "gkapfham/vim-vitamin-onec",
+    lazy = false,
+    priority = 1000,
+    config = function()
+      vim.cmd([[colorscheme vitaminonec]])
+    end,
+  },
+
+}
