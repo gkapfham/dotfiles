51fe8f96b38d3defdb7cfbc7ec46e915afe73423
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2023-01-04 23:09:03 -0500

    Delete a function that was not needed in config function in completion.lua for nvim-cmp.

--- nvim/lua/plugins/completion.lua
+++ nvim/lua/plugins/completion.lua
@@ -88,16 +88,6 @@ return {
     config = function()
       local cmp = require("cmp")
       local luasnip = require("luasnip")
-
-	-- Define the has_words_before function used in the
-	-- integration between luasnip and nvim-cmp; note
-	-- that this function must exist for other code in
-	-- this file to work correctly
-	local has_words_before = function()
-	  local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-	  return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
-	end
-
 	cmp.setup({
 	  -- Define the performance characteristics for nvim-cmp
 	  performance = {
