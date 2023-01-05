8146bc255aa0fcf5c8425324f29b1423c3140281
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2023-01-04 21:41:50 -0500

    Update the versioncontrol.lua plugin with more mappings and better comments.

--- nvim/lua/plugins/versioncontrol.lua
+++ nvim/lua/plugins/versioncontrol.lua
@@ -1,19 +1,22 @@
 return {
 
-  -- fugitive
+  -- Fugitive
   {
     "tpope/vim-fugitive",
     cmd = {"G", "Git", "Gwrite"},
     keys = {
-      -- git status
+      -- Keys: git status
       { "<Space>gg", ":Git <CR>", desc = "Fugitive: Git status" },
       { "<leader>gg", ":Git <CR>", desc = "Fugitive: Git status" },
-      -- git write 
+      -- Keys: git write 
       { "<Space>gw", ":Gwrite <CR>", desc = "Fugitive: Git write to add file" },
       { "<leader>gw", ":Gwrite <CR>", desc = "Fugitive: Git write to add file" },
-      -- git commit
+      -- Keys: git commit
+      { "<Space>gcc", ":Git commit <CR>", desc = "Fugitive: Commit current hunk" },
       { "<leader>gcc", ":Git commit <CR>", desc = "Fugitive: Commit current hunk" },
+      { "<Space>gcf", ":Git commit %<CR>", desc = "Fugitive: Commit current file" },
       { "<leader>gcf", ":Git commit %<CR>", desc = "Fugitive: Commit current file" },
+      { "<Space>gca", ":Git commit -a<CR>", desc = "Fugitive: Commit all files" },
       { "<leader>gca", ":Git commit -a<CR>", desc = "Fugitive: Commit all files" },
     }
   }
