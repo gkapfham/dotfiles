9ad87a9b3e316fa8ed4b15a5e7c871ce1a4475ff
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2023-01-05 12:12:03 -0500

    Add the comments.lua file with setup for Comment.nvim plugin.

---
+++ nvim/lua/plugins/comments.lua
@@ -0,0 +1,18 @@
+-- File: plugin/comments.lua
+-- Purpose: Configure the comment plugin
+-- and keymaps for the plugin
+
+return {
+
+  -- Comments plugin
+  {
+    "numToStr/Comment.nvim",
+    event = "BufReadPre",
+    config = function()
+      local comment = require("Comment")
+      comment.setup()
+      vim.cmd([[nmap <Space>cc :execute "normal! i" . split(&commentstring, '%s')[0]<CR>]])
+    end,
+  },
+
+}
