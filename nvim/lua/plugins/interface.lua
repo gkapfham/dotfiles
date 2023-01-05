dc1a6ef48a62f4556a863be4779e90fc91098b45
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2023-01-04 22:46:47 -0500

    Add plugins/interface.lua with starting plugin installation and configuration.

---
+++ nvim/lua/plugins/interface.lua
@@ -0,0 +1,46 @@
+return {
+
+  -- better vim.notify
+  {
+    "rcarriga/nvim-notify",
+    keys = {
+      {
+        "<leader>nd",
+        function()
+          require("notify").dismiss({ silent = true, pending = true })
+        end,
+        desc = "Delete all Notifications",
+      },
+    },
+    config = {
+      timeout = 3000,
+      max_height = function()
+        return math.floor(vim.o.lines * 0.75)
+      end,
+      max_width = function()
+        return math.floor(vim.o.columns * 0.75)
+      end,
+    },
+  },
+
+  -- better vim.ui
+  {
+    "stevearc/dressing.nvim",
+    init = function()
+      vim.ui.select = function(...)
+        require("lazy").load({ plugins = { "dressing.nvim" } })
+        return vim.ui.select(...)
+      end
+      vim.ui.input = function(...)
+        require("lazy").load({ plugins = { "dressing.nvim" } })
+        return vim.ui.input(...)
+      end
+    end,
+  },
+
+  -- icons
+  "nvim-tree/nvim-web-devicons",
+
+  -- ui components
+  "MunifTanjim/nui.nvim",
+}
