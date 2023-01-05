d5b8cbfbefa373e80dd958b3c0cc7033edb487ec
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2023-01-04 22:07:59 -0500

    Add the configuraiton for gitsigns to the versioncontrol.lua file.

--- nvim/lua/plugins/versioncontrol.lua
+++ nvim/lua/plugins/versioncontrol.lua
@@ -19,6 +19,54 @@ return {
       { "<Space>gca", ":Git commit -a<CR>", desc = "Fugitive: Commit all files" },
       { "<leader>gca", ":Git commit -a<CR>", desc = "Fugitive: Commit all files" },
     }
-  }
+  },
+
+  -- Gitsigns
+  {
+    "lewis6991/gitsigns.nvim",
+    event = "BufReadPre",
+    config = function()
+	require('gitsigns').setup {
+	  signs = {
+	    add          = {hl = 'DiffAdd'   , text = '+', numhl='None', linehl='None'},
+	    change       = {hl = 'DiffChange', text = '~', numhl='None', linehl='None'},
+	    delete       = {hl = 'DiffDelete', text = '-', numhl='None', linehl='None'},
+	    topdelete    = {hl = 'DiffDelete', text = '^', numhl='None', linehl='None'},
+	    changedelete = {hl = 'DiffChange', text = '~', numhl='None', linehl='None'},
+	  },
+	  numhl = false,
+	  linehl = false,
+	  keymaps = {
+	    -- Default keymap options
+	    noremap = true,
+	    buffer = true,
+	    ['n ]c'] = { expr = true, "&diff ? ']c' : '<cmd>lua require\"gitsigns\".next_hunk()<CR>'"},
+	    ['n [c'] = { expr = true, "&diff ? '[c' : '<cmd>lua require\"gitsigns\".prev_hunk()<CR>'"},
+	    ['n <leader>hs'] = '<cmd>lua require"gitsigns".stage_hunk()<CR>',
+	    ['n <leader>hu'] = '<cmd>lua require"gitsigns".undo_stage_hunk()<CR>',
+	    ['n <leader>hr'] = '<cmd>lua require"gitsigns".reset_hunk()<CR>',
+	    ['n <leader>hR'] = '<cmd>lua require"gitsigns".reset_buffer()<CR>',
+	    ['n <leader>hp'] = '<cmd>lua require"gitsigns".preview_hunk()<CR>',
+	    ['n <leader>hb'] = '<cmd>lua require"gitsigns".blame_line()<CR>',
+	    -- Text objects
+	    ['o ih'] = ':<C-U>lua require"gitsigns".select_hunk()<CR>',
+	    ['x ih'] = ':<C-U>lua require"gitsigns".select_hunk()<CR>'
+	  },
+	  watch_gitdir = {
+	    interval = 500
+	  },
+	  diff_opts = {
+	    internal = true
+	  },
+	  attach_to_untracked = false,
+	  current_line_blame = false,
+	  sign_priority = 100,
+	  update_debounce = 50,
+	  status_formatter = nil,
+	}
+    end,
+  },
+
+
 
 }
