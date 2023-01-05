e38193075e2e14defc9342e957d3a815ac45719f
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2023-01-05 16:43:13 -0500

    Add more settings to settings.lua for vim.wo and then add comments.

--- nvim/lua/configure/settings.lua
+++ nvim/lua/configure/settings.lua
@@ -1,8 +1,21 @@
 -- Configure variables that are a part of neovim's global environment
 
+-- Define settings for vim.opt variables {{{
+
 -- Define the signcolumn
 vim.opt.signcolumn="yes:1"
 
 -- Disable the welcome message
 vim.opt.shortmess="FIWacto"
 
+-- }}}
+
+-- Display settings for vim.wo variables {{{
+
+-- Display numbers
+vim.wo.number=true
+
+-- Display relative numbers
+vim.wo.relativenumber=true
+
+--- }}}
