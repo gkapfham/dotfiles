2af937c320df4baf1f607abe3f085a10a9561a3f
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2023-01-04 22:46:24 -0500

    Add starting version of plugins/diagnostics.lua with vim-startuptime and plenary.nvim.

---
+++ nvim/lua/plugins/diagnostics.lua
@@ -0,0 +1,16 @@
+return {
+
+  -- measure the startuptime
+  {
+    "dstein64/vim-startuptime",
+    cmd = "StartupTime",
+    config = function()
+      vim.g.startuptime_tries = 10
+    end,
+  },
+
+  -- library used by other plugins
+  "nvim-lua/plenary.nvim",
+
+}
+
