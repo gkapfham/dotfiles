fe8f6ec90faed147efe05ee434a00a420bb7001c
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2023-01-05 21:11:56 -0500

    Add more settings for tabs and linebreaks in the settings.lua.

--- nvim/lua/configure/settings.lua
+++ nvim/lua/configure/settings.lua
@@ -18,4 +18,23 @@ vim.cmd([[set number]])
 -- Display relative numbers
 vim.cmd([[set relativenumber]])
 
+-- }}}
+
+-- Display settings through use of vim.cmd and then vimscript commands {{{
+
+-- Display linebreaks and tabs
+vim.cmd([[
+set linebreak
+set showbreak=━━
+set breakindent
+set tabstop=4
+]])
+
+-- Insert spaces for a tab
+vim.cmd([[
+set expandtab
+set smarttab
+set shiftround
+]])
+
 --- }}}
