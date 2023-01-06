dac0ffe36e0c008a90e55d2b0e829d9bf8036cce
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2023-01-06 14:46:43 -0500

    Add another mapping for the localleader in the init.lua file.

--- nvim/init.lua
+++ nvim/init.lua
@@ -14,6 +14,7 @@ vim.opt.rtp:prepend(lazypath)
 
 -- Define the leader key
 vim.g.mapleader = ","
+vim.g.maplocalleader = ","
 
 -- Setup the lazy.nvim to search for lua files
 -- in the plugins/ directory, load the color
