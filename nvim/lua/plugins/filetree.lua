33f5c71fb75109de5bf761f5e75cc072f758160a
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2023-01-06 15:16:22 -0500

    Reformat and remove trailing spaces in the filetree.lua.

--- nvim/lua/plugins/filetree.lua
+++ nvim/lua/plugins/filetree.lua
@@ -9,182 +9,182 @@ return {
     cmd = "NeoTreeShowToggle",
     -- Configure
     config = function()
-	local nvim_web_devicons = require "nvim-web-devicons"
-	local current_icons = nvim_web_devicons.get_icons()
-	local new_icons = {}
-	for key, icon in pairs(current_icons) do
-	    icon.color = "#a8a8a8"
-	    new_icons[key] = icon
-	end
-	nvim_web_devicons.set_icon(new_icons)
-	nvim_web_devicons.set_default_icon('', '#a8a8a8')
-	require("neo-tree").setup({
-		close_if_last_window = false,
-		popup_border_style = "rounded",
-		enable_git_status = true,
-		enable_diagnostics = true,
-		sort_case_insensitive = false,
-		sort_function = nil,
-		default_component_configs = {
-		  container = {
-		    enable_character_fade = true
-		  },
-		  indent = {
-		    indent_size = 1,
-		    padding = 1,
-		    with_markers = true,
-		    indent_marker = "│",
-		    last_indent_marker = "└",
-		    highlight = "NeoTreeIndentMarker",
-		    with_expanders = nil,
-		    expander_collapsed = "",
-		    expander_expanded = "",
-		    expander_highlight = "NeoTreeExpander",
-		  },
-		  icon = {
-		    folder_closed = "",
-		    folder_open = "",
-		    folder_empty = "ﰊ",
-		    default = "*",
-		    highlight = "NeoTreeFileIcon"
-		  },
-		  modified = {
-		    symbol = "",
-		    highlight = "NeoTreeModified",
-		  },
-		  name = {
-		    trailing_slash = false,
-		    use_git_status_colors = false,
-		    highlight = "NeoTreeFileName",
-		  },
-		  git_status = {
-		    symbols = {
-		      -- Change type
-		      added     = "",
-		      modified  = "",
-		      deleted   = "",
-		      renamed   = "",
-		      -- Status type
-		      untracked = "",
-		      ignored   = "",
-		      unstaged  = "",
-		      staged    = "",
-		      conflict  = "",
-		    }
-		  },
-		},
-		window = {
-		  position = "right",
-		  width = "20%",
-		  mapping_options = {
-		    noremap = true,
-		    nowait = true,
-		  },
-		  mappings = {
-		    ["<space>"] = {
-			"toggle_node",
-			nowait = false,
-		    },
-		    ["<2-LeftMouse>"] = "open",
-		    ["<cr>"] = "open",
-		    ["<esc>"] = "revert_preview",
-		    ["P"] = { "toggle_preview", config = { use_float = false } },
-		    ["S"] = "open_split",
-		    ["s"] = "open_vsplit",
-		    ["t"] = "open_tabnew",
-		    ["w"] = "open_with_window_picker",
-		    ["C"] = "close_node",
-		    ["z"] = "close_all_nodes",
-		    ["Z"] = "expand_all_nodes",
-		    ["a"] = {
-		      "add",
-		      config = {
-			show_path = "none" -- "none", "relative", "absolute"
-		      }
-		    },
-		    ["A"] = "add_directory",
-		    ["d"] = "delete",
-		    ["r"] = "rename",
-		    ["y"] = "copy_to_clipboard",
-		    ["x"] = "cut_to_clipboard",
-		    ["p"] = "paste_from_clipboard",
-		    ["c"] = "copy",
-		    ["m"] = "move",
-		    ["q"] = "close_window",
-		    ["R"] = "refresh",
-		    ["?"] = "show_help",
-		    ["<"] = "prev_source",
-		    [">"] = "next_source",
-		  }
-		},
-		nesting_rules = {},
-		filesystem = {
-		  filtered_items = {
-		    visible = false,
-		    hide_dotfiles = true,
-		    hide_gitignored = true,
-		    hide_hidden = true,
-		    hide_by_name = {
-		    },
-		    hide_by_pattern = {
-		    },
-		    always_show = {
-		    },
-		    never_show = {
-		    },
-		    never_show_by_pattern = {
-		    },
-		  },
-		  follow_current_file = true,
-		  group_empty_dirs = false,
-		  hijack_netrw_behavior = "open_default",
-		  use_libuv_file_watcher = true,
-		  window = {
-		    mappings = {
-		      ["-"] = "navigate_up",
-		      ["."] = "set_root",
-		      ["H"] = "toggle_hidden",
-		      ["/"] = "fuzzy_finder",
-		      ["D"] = "fuzzy_finder_directory",
-		      ["f"] = "filter_on_submit",
-		      ["<c-x>"] = "clear_filter",
-		      ["[g"] = "prev_git_modified",
-		      ["]g"] = "next_git_modified",
-		    }
-		  }
-		},
-		buffers = {
-		  follow_current_file = true,
-		  group_empty_dirs = true,
-		  show_unloaded = true,
-		  window = {
-		    mappings = {
-		      ["bd"] = "buffer_delete",
-		      ["<bs>"] = "navigate_up",
-		      ["."] = "set_root",
-		    }
-		  },
-		},
-		git_status = {
-		  window = {
-		    position = "float",
-		    mappings = {
-		      ["A"]  = "git_add_all",
-		      ["gu"] = "git_unstage_file",
-		      ["ga"] = "git_add_file",
-		      ["gr"] = "git_revert_file",
-		      ["gc"] = "git_commit",
-		      ["gp"] = "git_push",
-		      ["gg"] = "git_commit_and_push",
-		    }
-		  }
-		}
-	      })
+      local nvim_web_devicons = require "nvim-web-devicons"
+      local current_icons = nvim_web_devicons.get_icons()
+      local new_icons = {}
+      for key, icon in pairs(current_icons) do
+        icon.color = "#a8a8a8"
+        new_icons[key] = icon
+      end
+      nvim_web_devicons.set_icon(new_icons)
+      nvim_web_devicons.set_default_icon('', '#a8a8a8')
+      require("neo-tree").setup({
+        close_if_last_window = false,
+        popup_border_style = "rounded",
+        enable_git_status = true,
+        enable_diagnostics = true,
+        sort_case_insensitive = false,
+        sort_function = nil,
+        default_component_configs = {
+          container = {
+            enable_character_fade = true
+          },
+          indent = {
+            indent_size = 1,
+            padding = 1,
+            with_markers = true,
+            indent_marker = "│",
+            last_indent_marker = "└",
+            highlight = "NeoTreeIndentMarker",
+            with_expanders = nil,
+            expander_collapsed = "",
+            expander_expanded = "",
+            expander_highlight = "NeoTreeExpander",
+          },
+          icon = {
+            folder_closed = "",
+            folder_open = "",
+            folder_empty = "ﰊ",
+            default = "*",
+            highlight = "NeoTreeFileIcon"
+          },
+          modified = {
+            symbol = "",
+            highlight = "NeoTreeModified",
+          },
+          name = {
+            trailing_slash = false,
+            use_git_status_colors = false,
+            highlight = "NeoTreeFileName",
+          },
+          git_status = {
+            symbols = {
+              -- Change type
+              added     = "",
+              modified  = "",
+              deleted   = "",
+              renamed   = "",
+              -- Status type
+              untracked = "",
+              ignored   = "",
+              unstaged  = "",
+              staged    = "",
+              conflict  = "",
+            }
+          },
+        },
+        window = {
+          position = "right",
+          width = "20%",
+          mapping_options = {
+            noremap = true,
+            nowait = true,
+          },
+          mappings = {
+            ["<space>"] = {
+              "toggle_node",
+              nowait = false,
+            },
+            ["<2-LeftMouse>"] = "open",
+            ["<cr>"] = "open",
+            ["<esc>"] = "revert_preview",
+            ["P"] = { "toggle_preview", config = { use_float = false } },
+            ["S"] = "open_split",
+            ["s"] = "open_vsplit",
+            ["t"] = "open_tabnew",
+            ["w"] = "open_with_window_picker",
+            ["C"] = "close_node",
+            ["z"] = "close_all_nodes",
+            ["Z"] = "expand_all_nodes",
+            ["a"] = {
+              "add",
+              config = {
+                show_path = "none" -- "none", "relative", "absolute"
+              }
+            },
+            ["A"] = "add_directory",
+            ["d"] = "delete",
+            ["r"] = "rename",
+            ["y"] = "copy_to_clipboard",
+            ["x"] = "cut_to_clipboard",
+            ["p"] = "paste_from_clipboard",
+            ["c"] = "copy",
+            ["m"] = "move",
+            ["q"] = "close_window",
+            ["R"] = "refresh",
+            ["?"] = "show_help",
+            ["<"] = "prev_source",
+            [">"] = "next_source",
+          }
+        },
+        nesting_rules = {},
+        filesystem = {
+          filtered_items = {
+            visible = false,
+            hide_dotfiles = true,
+            hide_gitignored = true,
+            hide_hidden = true,
+            hide_by_name = {
+            },
+            hide_by_pattern = {
+            },
+            always_show = {
+            },
+            never_show = {
+            },
+            never_show_by_pattern = {
+            },
+          },
+          follow_current_file = true,
+          group_empty_dirs = false,
+          hijack_netrw_behavior = "open_default",
+          use_libuv_file_watcher = true,
+          window = {
+            mappings = {
+              ["-"] = "navigate_up",
+              ["."] = "set_root",
+              ["H"] = "toggle_hidden",
+              ["/"] = "fuzzy_finder",
+              ["D"] = "fuzzy_finder_directory",
+              ["f"] = "filter_on_submit",
+              ["<c-x>"] = "clear_filter",
+              ["[g"] = "prev_git_modified",
+              ["]g"] = "next_git_modified",
+            }
+          }
+        },
+        buffers = {
+          follow_current_file = true,
+          group_empty_dirs = true,
+          show_unloaded = true,
+          window = {
+            mappings = {
+              ["bd"] = "buffer_delete",
+              ["<bs>"] = "navigate_up",
+              ["."] = "set_root",
+            }
+          },
+        },
+        git_status = {
+          window = {
+            position = "float",
+            mappings = {
+              ["A"]  = "git_add_all",
+              ["gu"] = "git_unstage_file",
+              ["ga"] = "git_add_file",
+              ["gr"] = "git_revert_file",
+              ["gc"] = "git_commit",
+              ["gp"] = "git_push",
+              ["gg"] = "git_commit_and_push",
+            }
+          }
+        }
+      })
     end,
     keys = {
       -- Activate
       { "<Space>0", "<cmd> NeoTreeShowToggle<CR>", desc = "Neo-tree: Activate" },
-    } 
-  }	
+    }
+  }
 
 }
