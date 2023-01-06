2974fe1be88a8f0d4cebae5791f492583bee10d9
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2023-01-05 19:22:45 -0500

    Add the treesitter.lua file with basic configuration.

---
+++ nvim/lua/plugins/treesitter.lua
@@ -0,0 +1,48 @@
+-- File: treesitter.lua
+-- Purpose: load and configure the treesitter
+-- that enables syntax highlighting and navigation
+
+return {
+
+  -- Treesitter
+  {
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+    event = "BufReadPost",
+    config = function()
+      require("nvim-treesitter.configs").setup({
+        sync_install = false,
+        ensure_installed = {
+          "bash",
+          "bibtex",
+          "c",
+          "gitattributes",
+          "gitcommit",
+          "gitignore",
+          "go",
+          "help",
+          "html",
+          "java",
+          "javascript",
+          "json",
+          "latex",
+          "lua",
+          "make",
+          "markdown",
+          "markdown_inline",
+          "python",
+          "query",
+          "regex",
+          "tsx",
+          "typescript",
+          "vim",
+          "yaml",
+        },
+        highlight = { enable = true },
+        indent = { enable = true },
+        context_commentstring = { enable = true, enable_autocmd = false },
+      })
+    end,
+  },
+
+}
