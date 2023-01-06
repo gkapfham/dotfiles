cd4cbfa72613568a0758328331e1fcf00e561c0e
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2023-01-06 15:09:49 -0500

    Add improved use of C-n and C-p in completion.lua.

--- nvim/lua/plugins/completion.lua
+++ nvim/lua/plugins/completion.lua
@@ -167,8 +167,32 @@ return {
               fallback()
             end
           end, { "i", "s" }),
+          -- Define the same <Tab> mapping but also for
+          -- <C-n> so that this also advances forward
+          ["<C-n>"] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+              cmp.select_next_item()
+            elseif luasnip.expand_or_jumpable() then
+              luasnip.expand_or_jump()
+            elseif has_words_before() then
+              cmp.complete()
+            else
+              fallback()
+            end
+          end, { "i", "s" }),
           -- Go back in the template holes in the snippet
           ["<S-Tab>"] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+              cmp.select_prev_item()
+            elseif luasnip.jumpable(-1) then
+              luasnip.jump(-1)
+            else
+              fallback()
+            end
+          end, { "i", "s" }),
+          -- Define the same <S-Tab> mapping but also for
+          -- <C-p> so that this also advances backward
+          ["<C-p>"] = cmp.mapping(function(fallback)
             if cmp.visible() then
               cmp.select_prev_item()
             elseif luasnip.jumpable(-1) then
