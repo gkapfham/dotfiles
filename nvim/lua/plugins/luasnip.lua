12676d32e8d316c48a84ec3d1dca4b5ba1f14289
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2023-01-07 19:46:01 -0500

    Add gitcommit luasnip specifications to the luasnip.lua file.

--- nvim/lua/plugins/luasnip.lua
+++ nvim/lua/plugins/luasnip.lua
@@ -16,41 +16,84 @@ return {
         local i = ls.insert_node
         -- Define snippets for email messages
         ls.add_snippets("mail", {
-            -- Signatures at the end of an email message
-            s({trig = "tyakr", dscr = "Detailed Email Sign-Off"}, {
-                t({"Thank You and Kind Regards,", "", "  Greg"}),
-            }),
-            s({trig = "taakr", dscr = "Detailed Email Sign-Off"}, {
-                t({"Thanks Again and Kind Regards,", "", "  Greg"}),
-            }),
-            s({trig = "kr", dscr = "Kind Regards"}, {
-                t({"Kind Regards,", "", "  Greg"}),
-            }),
-            s({trig = "ty", dscr = "Thank You"}, {
-                t({"Thank You,", "", "  Greg"}),
-            }),
-            -- Greetings at the start of an email message
-            s({trig = "hac", dscr = "Hello Again Colleagues"}, {
-                t({"Hello Again Colleagues"}),
-                t({",", "", ""}),
-            }),
-            s({trig = "hc", dscr = "Hello Colleagues"}, {
-                t({"Hello Colleagues"}),
-                t({",", "", ""}),
-            }),
-            s({trig = "h", dscr = "Hello"}, {
-                t({"Hello "}),
-                i(1, {"Name"}),
-                t({",", "", ""}),
-                }),
-            s({trig = "ha", dscr = "Hello Again"}, {
-                  t({"Hello Again "}),
-                  i(1, {"Name"}),
-                  t({",", "", ""}),
-                  })
-          },
+          -- Signatures at the end of an email message
+          s({trig = "tyakr", dscr = "Detailed Email Sign-Off"}, {
+            t({"Thank You and Kind Regards,", "", "  Greg"}),
+          }),
+          s({trig = "taakr", dscr = "Detailed Email Sign-Off"}, {
+            t({"Thanks Again and Kind Regards,", "", "  Greg"}),
+          }),
+          s({trig = "kr", dscr = "Kind Regards"}, {
+            t({"Kind Regards,", "", "  Greg"}),
+          }),
+          s({trig = "ty", dscr = "Thank You"}, {
+            t({"Thank You,", "", "  Greg"}),
+          }),
+          -- Greetings at the start of an email message
+          s({trig = "hac", dscr = "Hello Again Colleagues"}, {
+            t({"Hello Again Colleagues"}),
+            t({",", "", ""}),
+          }),
+          s({trig = "hc", dscr = "Hello Colleagues"}, {
+            t({"Hello Colleagues"}),
+            t({",", "", ""}),
+          }),
+          s({trig = "h", dscr = "Hello"}, {
+            t({"Hello "}),
+            i(1, {"Name"}),
+            t({",", "", ""}),
+          }),
+          s({trig = "ha", dscr = "Hello Again"}, {
+            t({"Hello Again "}),
+            i(1, {"Name"}),
+            t({",", "", ""}),
+          })
+        },
           -- Define the type of signatures in this table
           {key = "mail",}
+        )
+        -- Define snippets for email messages
+        ls.add_snippets("gitcommit", {
+          -- Simplified versions of conventional commits
+          s({trig = "break", dscr = "break: conventional commits"}, {
+            t({"break: "}),
+            i(1, {"Message"}),
+          }),
+          s({trig = "chore", dscr = "chore: conventional commits"}, {
+            t({"chore: "}),
+            i(1, {"Message"}),
+          }),
+          s({trig = "docs", dscr = "docs: conventional commits"}, {
+            t({"docs: "}),
+            i(1, {"Message"}),
+          }),
+          s({trig = "feat", dscr = "feat: conventional commits"}, {
+            t({"feat: "}),
+            i(1, {"Message"}),
+          }),
+          s({trig = "fix", dscr = "fix: conventional commits"}, {
+            t({"fix: "}),
+            i(1, {"Message"}),
+          }),
+          s({trig = "refactor", dscr = "refactor: conventional commits"}, {
+            t({"refactor: "}),
+            i(1, {"Message"}),
+          }),
+          s({trig = "style", dscr = "style: conventional commits"}, {
+            t({"style: "}),
+            i(1, {"Message"}),
+          }),
+          s({trig = "test", dscr = "test: conventional commits"}, {
+            t({"test: "}),
+            i(1, {"Message"}),
+          }),
+          s({trig = "try", dscr = "try: personalized conventional commits"}, {
+            t({"try: "}),
+            i(1, {"Message"}),
+          }),
+        },
+          -- Define the type of signatures in this table
+          {key = "gitcommit",}
         )
       end,
     },
