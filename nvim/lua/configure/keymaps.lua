509090abde41e0dc7f12c10cfc6de1ec4e0dd784
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2023-01-07 19:38:09 -0500

    Impove the comments in the keymaps.lua file.

--- nvim/lua/configure/keymaps.lua
+++ nvim/lua/configure/keymaps.lua
@@ -1,27 +1,28 @@
--- Define keymaps not specifically connected to a plugin
+-- File: configure/keymaps.lua
+-- Purpose: define keymaps not specifically connected to a plugin
 
 -- Use the nvim API
 
 -- Insert mode key mappings {{{
 
--- remap escape
+-- Remap escape
 vim.keymap.set("i", "jk", "<ESC>", { desc = "Custom keymap: Escape" })
 
 -- }}}
 
 -- Normal mode key mappings {{{
 
--- buffers
+-- Buffers
 vim.keymap.set("n", "<Space>u", "<cmd>b#<cr>", { desc = "Custom keymap: Previous buffer" })
 
--- command mode
+-- Command mode
 vim.keymap.set("n", "<leader><leader>", ":", { desc = "Custom keymap: Enter command mode" })
 
--- lines
+-- Lines
 vim.keymap.set("n", "oo", "O<ESC>", { desc = "Custom keymap: Insert a blank line at end of line" })
 vim.keymap.set("n", "oO", "i<cr><ESC>", { desc = "Custom keymap: Insert a blank line at the cursor location" })
 
--- quitting
+-- Quitting
 vim.keymap.set("n", "<leader>qq", "<cmd>qa<cr>", { desc = "Custom keymap: Quit all" })
 vim.keymap.set("n", "<Space>qq", "<cmd>qa<cr>", { desc = "Custom keymap: Quit all" })
 
@@ -32,13 +33,11 @@ vim.keymap.set("n", "<Space>qq", "<cmd>qa<cr>", { desc = "Custom keymap: Quit al
 vim.cmd([[
 " Remove trailing whitespace
 nnoremap <leader>rtw :%s/\s\+$//e<CR>
-
 " Fix a misspelling with next-best word
 nmap <silent> zn <Plug>(SpellRotateForward)
 nmap <silent> zp <Plug>(SpellRotateBackward)
 vmap <silent> zn <Plug>(SpellRotateForwardV)
 vmap <silent> zp <Plug>(SpellRotateBackwardV)
-
 " Toggle the display of spelling mistakes
 nmap <leader>s :set spell!<CR>
 ]])
