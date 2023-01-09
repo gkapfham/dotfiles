b1405f969c74a073272ca34a250eabf09a166d61
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2023-01-08 22:39:21 -0500

    Add the autopairs.lua file in the plugins/ directory.

---
+++ nvim/lua/plugins/autopairs.lua
@@ -0,0 +1,56 @@
+-- File: plugins/autopairs.lua
+-- Purpose: Configure the autopairs plugin
+
+return {
+
+  -- Autopairs
+  {
+    "windwp/nvim-autopairs",
+    event="VeryLazy",
+    config = function()
+      local remap = vim.api.nvim_set_keymap
+      local npairs = require('nvim-autopairs')
+      npairs.setup({ map_bs = false, map_cr = false })
+      vim.g.coq_settings = { keymap = { recommended = false } }
+      -- skip it, if you use another global object
+      _G.MUtils= {}
+      MUtils.CR = function()
+        if vim.fn.pumvisible() ~= 0 then
+          if vim.fn.complete_info({ 'selected' }).selected ~= -1 then
+            return npairs.esc('<c-y>')
+          else
+            return npairs.esc('<c-e>') .. npairs.autopairs_cr()
+          end
+        else
+          return npairs.autopairs_cr()
+        end
+      end
+      remap('i', '<cr>', 'v:lua.MUtils.CR()', { expr = true, noremap = true })
+      MUtils.BS = function()
+        if vim.fn.pumvisible() ~= 0 and vim.fn.complete_info({ 'mode' }).mode == 'eval' then
+          return npairs.esc('<c-e>') .. npairs.autopairs_bs()
+        else
+          return npairs.autopairs_bs()
+        end
+      end
+      remap('i', '<bs>', 'v:lua.MUtils.BS()', { expr = true, noremap = true })
+      local cmp_autopairs = require('nvim-autopairs.completion.cmp')
+      local cmp = require('cmp')
+      cmp.event:on(
+        'confirm_done',
+        cmp_autopairs.on_confirm_done()
+      )
+    end,
+  },
+
+  -- Autotag
+  {
+    "windwp/nvim-ts-autotag",
+    event = "VeryLazy",
+    config = function()
+      require('nvim-ts-autotag').setup()
+    end
+  },
+
+
+}
