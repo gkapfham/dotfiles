17851bea10ea0c73418f363a6b3274569304627e
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2023-01-09 00:16:10 -0500

    Add the use of the mini.surround plugin extras.lua.

--- nvim/lua/plugins/extras.lua
+++ nvim/lua/plugins/extras.lua
@@ -9,4 +9,24 @@ return {
     cmd = "MundoToggle",
   },
 
+  -- Surround
+  {
+    "echasnovski/mini.surround",
+    keys = { "gz" },
+    config = function()
+      -- use gz mappings instead of s to prevent conflict with leap
+      require("mini.surround").setup({
+        mappings = {
+          add = "gza",
+          delete = "gzd",
+          find = "gzf",
+          find_left = "gzF", -- Find surrounding (to the left)
+          highlight = "gzh", -- Highlight surrounding
+          replace = "gzr", -- Replace surrounding
+          update_n_lines = "gzn", -- Update `n_lines`
+        },
+      })
+    end,
+  },
+
 }
