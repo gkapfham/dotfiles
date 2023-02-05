b4eb252d95c8700ea7a2de91afb288958b8d1857
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2023-02-04 20:37:50 -0500

    Try out the tree-climber.nvim instead of a Python solution in treesitter.lua.

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -22,11 +22,28 @@ return {
     event = "VeryLazy"
   },
 
-  -- Textobjects and motions for Python
+  -- Tree-climber
   {
-    "jeetsukumaran/vim-pythonsense",
-    event = "VeryLazy"
+    "drybalka/tree-climber.nvim",
+    event = "VeryLazy",
+    config = function()
+      local keyopts = { noremap = true, silent = true }
+      vim.keymap.set({'n', 'v', 'o'}, 'H', require('tree-climber').goto_parent, keyopts)
+      vim.keymap.set({'n', 'v', 'o'}, 'L', require('tree-climber').goto_child, keyopts)
+      vim.keymap.set({'n', 'v', 'o'}, 'J', require('tree-climber').goto_next, keyopts)
+      vim.keymap.set({'n', 'v', 'o'}, 'K', require('tree-climber').goto_prev, keyopts)
+      vim.keymap.set({'v', 'o'}, 'in', require('tree-climber').select_node, keyopts)
+      vim.keymap.set('n', '<c-k>', require('tree-climber').swap_prev, keyopts)
+      vim.keymap.set('n', '<c-j>', require('tree-climber').swap_next, keyopts)
+      vim.keymap.set('n', '<c-h>', require('tree-climber').highlight_node, keyopts)
+    end
   },
+
+  -- -- Textobjects and motions for Python
+  -- {
+  --   "jeetsukumaran/vim-pythonsense",
+  --   event = "VeryLazy"
+  -- },
 
   -- Improve indentation that addresses
   -- the limitation of treesitter for Python
