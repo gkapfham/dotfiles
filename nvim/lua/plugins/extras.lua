f4de49231ee96f16942a4db095ef8c7568e71585
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2023-02-22 15:52:27 -0500

    Add the setup for mini.bracketed in the extras.lua file.

--- nvim/lua/plugins/extras.lua
+++ nvim/lua/plugins/extras.lua
@@ -29,6 +29,31 @@ return {
     end,
   },
 
+  -- Bracketed
+  {
+    "echasnovski/mini.bracketed",
+    event = "VeryLazy",
+    config = function()
+      require("mini.bracketed").setup({
+        buffer     = { suffix = 'b', options = {} },
+        comment    = { suffix = 'e', options = {} },
+        conflict   = { suffix = 'x', options = {} },
+        diagnostic = { suffix = 'd', options = {} },
+        file       = { suffix = 'f', options = {} },
+        indent     = { suffix = 'i', options = {} },
+        jump       = { suffix = 'j', options = {} },
+        location   = { suffix = 'l', options = {} },
+        oldfile    = { suffix = 'o', options = {} },
+        quickfix   = { suffix = 'q', options = {} },
+        treesitter = { suffix = 't', options = {} },
+        undo       = { suffix = 'u', options = {} },
+        window     = { suffix = 'w', options = {} },
+        yank       = { suffix = 'y', options = {} },
+      }
+      )
+    end,
+  },
+
   -- Align
   {
     "echasnovski/mini.align",
