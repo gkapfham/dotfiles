b335bd7de03358437ddbfff92734d7cd048a6f1c
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2023-04-03 12:03:33 -0400

    Add more configuration details to the telescope.lua file.

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -74,14 +74,14 @@ return {
         extensions = {
           undo = {
             use_delta = false,
-            use_custom_command = nil,
             side_by_side = false,
+            use_custom_command = nil,
             diff_context_lines = vim.o.scrolloff,
             entry_format = "state #$ID, $STAT, $TIME",
             mappings = {
               i = {
                 ["<cr>"] = require("telescope-undo.actions").yank_additions,
-                ["<S-cr>"] = require("telescope-undo.actions").yank_deletions,
+                ["<C-cr>"] = require("telescope-undo.actions").yank_deletions,
                 ["<C-r>"] = require("telescope-undo.actions").restore,
               },
             },
