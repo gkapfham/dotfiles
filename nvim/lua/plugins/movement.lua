e47b27607cfd8aa035cfc3ba3eeef5e0341e22b5
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2023-06-22 11:24:17 -0400

    Disable the use of which-key.nvim as it interferes with workflow.

--- nvim/lua/plugins/movement.lua
+++ nvim/lua/plugins/movement.lua
@@ -146,21 +146,21 @@ return {
     },
   },
 
-  -- Display diagnostic information about different keymaps,
-  -- including information about the clipboard and spelling
-  {
-    "folke/which-key.nvim",
-    event = "VeryLazy",
-    init = function()
-      vim.o.timeout = true
-      vim.o.timeoutlen = 300
-    end,
-    opts = {
-      -- your configuration comes here
-      -- or leave it empty to use the default settings
-      -- refer to the configuration section below
-    }
-  },
+  -- -- Display diagnostic information about different keymaps,
+  -- -- including information about the clipboard and spelling
+  -- {
+  --   "folke/which-key.nvim",
+  --   event = "VeryLazy",
+  --   init = function()
+  --     vim.o.timeout = true
+  --     vim.o.timeoutlen = 300
+  --   end,
+  --   opts = {
+  --     -- your configuration comes here
+  --     -- or leave it empty to use the default settings
+  --     -- refer to the configuration section below
+  --   }
+  -- },
 
   -- Pair movement and highlighting
   -- (note could not get vim-matchup to work;
