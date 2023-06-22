feffff401ea6ff66867c58dae3df1bd8cf39054d
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2023-06-21 22:41:21 -0400

    Add the use of flash.nvim and which-key.nvim to movement.lua.

--- nvim/lua/plugins/movement.lua
+++ nvim/lua/plugins/movement.lua
@@ -3,18 +3,158 @@
 
 return {
 
-  -- Leap and flit
+  -- -- Leap and flit
+  -- {
+  --   "ggandor/leap.nvim",
+  --   event = "VeryLazy",
+  --   dependencies = { {"ggandor/flit.nvim", config = {
+  --     multiline = true,
+  --     eager_ops = true,
+  --     keymaps = { f = 'f', F = 'F', t = 't', T = 'T' }
+  --   } } },
+  --   config = function()
+  --     require("leap").add_default_mappings(true)
+  --   end,
+  -- },
+
   {
-    "ggandor/leap.nvim",
+    "folke/flash.nvim",
     event = "VeryLazy",
-    dependencies = { {"ggandor/flit.nvim", config = {
-      multiline = true,
-      eager_ops = true,
-      keymaps = { f = 'f', F = 'F', t = 't', T = 'T' }
-    } } },
-    config = function()
-      require("leap").add_default_mappings(true)
+    ---@type Flash.Config
+    opts = {
+      -- labels = "abcdefghijklmnopqrstuvwxyz",
+      labels = "asdfghjklqwertyuiopzxcvbnm",
+      search = {
+        -- search/jump in all windows
+        multi_window = true,
+        -- search direction
+        forward = true,
+        -- when `false`, find only matches in the given direction
+        wrap = true,
+        ---@type Flash.Pattern.Mode
+        -- Each mode will take ignorecase and smartcase into account.
+        -- * exact: exact match
+        -- * search: regular search
+        -- * fuzzy: fuzzy search
+        -- * fun(str): custom function that returns a pattern
+        --   For example, to only match at the beginning of a word:
+        --   mode = function(str)
+        --     return "\\<" .. str
+        --   end,
+        mode = "exact",
+        -- behave like `incsearch`
+        incremental = false,
+        filetype_exclude = { "notify", "noice" },
+      },
+      jump = {
+        -- save location in the jumplist
+        jumplist = true,
+        -- jump position
+        pos = "start", ---@type "start" | "end" | "range"
+        -- add pattern to search history
+        history = true,
+        -- add pattern to search register
+        register = true,
+        -- clear highlight after jump
+        nohlsearch = false,
+        -- automatically jump when there is only one match
+        autojump = false,
+      },
+      highlight = {
+        label = {
+          -- add a label for the first match in the current window.
+          -- you can always jump to the first match with `<CR>`
+          current = true,
+          -- show the label after the match
+          after = true, ---@type boolean|number[]
+          -- show the label before the match
+          before = false, ---@type boolean|number[]
+          -- position of the label extmark
+          style = "overlay", ---@type "eol" | "overlay" | "right_align" | "inline"
+          -- flash tries to re-use labels that were already assigned to a position,
+          -- when typing more characters. By default only lower-case labels are re-used.
+          reuse = "lowercase", ---@type "lowercase" | "all"
+        },
+        -- show a backdrop with hl FlashBackdrop
+        backdrop = false,
+        -- Highlight the search matches
+        matches = true,
+        -- extmark priority
+        priority = 5000,
+        groups = {
+          match = "FlashMatch",
+          current = "FlashCurrent",
+          backdrop = "FlashBackdrop",
+          label = "FlashLabel",
+        },
+      },
+      -- action to perform when picking a label.
+      -- defaults to the jumping logic depending on the mode.
+      ---@type fun(match:Flash.Match, state:Flash.State)|nil
+      action = nil,
+      -- initial pattern to use when opening flash
+      pattern = "",
+      -- You can override the default options for a specific mode.
+      -- Use it with `require("flash").jump({mode = "forward"})`
+      ---@type table<string, Flash.Config>
+      modes = {
+        -- options used when flash is activated through
+        -- a regular search with `/` or `?`
+        search = {
+          enabled = true, -- enable flash for search
+          highlight = { backdrop = false },
+          jump = { history = true, register = true, nohlsearch = true },
+          search = {
+            -- `forward` will be automatically set to the search direction
+            -- `mode` is always set to `search`
+            -- `incremental` is set to `true` when `incsearch` is enabled
+          },
+        },
+        -- options used when flash is activated through
+        -- `f`, `F`, `t`, `T`, `;` and `,` motions
+        char = {
+          enabled = true,
+          search = { wrap = false },
+          highlight = { backdrop = false },
+          jump = { register = false },
+        },
+        -- options used for treesitter selections
+        -- `require("flash").treesitter()`
+        treesitter = {
+          labels = "abcdefghijklmnopqrstuvwxyz",
+          jump = { pos = "range" },
+          highlight = {
+            label = { before = true, after = true, style = "inline" },
+            backdrop = false,
+            matches = true,
+          },
+        },
+      },
+    },
+    keys = {
+      {
+        "s",
+        mode = { "n", "x", "o" },
+        function()
+          -- default options: exact mode, multi window, all directions, with a backdrop
+          require("flash").jump()
+        end,
+      },
+    },
+  },
+
+  {
+    "folke/which-key.nvim",
+    event = "VeryLazy",
+    init = function()
+      vim.o.timeout = true
+      vim.o.timeoutlen = 300
     end,
+    opts = {
+      -- your configuration comes here
+      -- or leave it empty to use the default settings
+      -- refer to the configuration section below
+    }
   },
 
   -- Pair movement and highlighting
