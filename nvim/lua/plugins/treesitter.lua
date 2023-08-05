37887f1e9096cf78c8909676ba22978aa61b42bc
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2023-08-05 13:45:26 -0400

    Fix a wording mistake in the comment in treesitter.lua.

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -46,6 +46,29 @@ return {
   --   event = "VeryLazy"
   -- },
 
+  -- indent-blankline.nvim
+  {
+    "nvim-treesitter/nvim-treesitter-context",
+    event = "VeryLazy",
+    config = function()
+      require'treesitter-context'.setup{
+        enable = true, -- Enable this plugin (Can be enabled/disabled later via commands)
+        max_lines = 0, -- How many lines the window should span. Values <= 0 mean no limit.
+        min_window_height = 0, -- Minimum editor window height to enable context. Values <= 0 mean no limit.
+        line_numbers = true,
+        multiline_threshold = 20, -- Maximum number of lines to collapse for a single context line
+        trim_scope = 'outer', -- Which context lines to discard if `max_lines` is exceeded. Choices: 'inner', 'outer'
+        mode = 'cursor',  -- Line used to calculate context. Choices: 'cursor', 'topline'
+        -- Separator between context and content. Should be a single character string, like '-'.
+        -- When separator is set, the context will only show up when there are at least 2 lines above cursorline.
+        separator = nil,
+        zindex = 20, -- The Z-index of the context window
+        on_attach = nil, -- (fun(buf: integer): boolean) return false to disable attaching
+      }
+    end,
+  },
+
+
   -- Treesitter
   {
     "nvim-treesitter/nvim-treesitter",
@@ -56,7 +79,7 @@ return {
     },
     config = function()
       -- rewrite deprecated function to the new function so
-      -- that all plugins who use the deprecated one do not
+      -- that all plugins that use the deprecated one do not
       -- produce the warning message about using wrong one
       local ts_utils = require("nvim-treesitter.ts_utils")
       ts_utils.is_in_node_range = vim.treesitter.is_in_node_range
