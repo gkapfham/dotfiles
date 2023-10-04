b1d88ad9fda51be1108e5aa864f21e98c2827d1f
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2023-10-04 14:42:20 -0400

    Delete the label section in movement.lua for flash.nvim as it belongs elsewhere.

--- nvim/lua/plugins/movement.lua
+++ nvim/lua/plugins/movement.lua
@@ -75,20 +75,6 @@ return {
         continue = true,
       },
       highlight = {
-        label = {
-          -- add a label for the first match in the current window.
-          -- you can always jump to the first match with `<CR>`
-          current = true,
-          -- show the label after the match
-          after = true, ---@type boolean|number[]
-          -- show the label before the match
-          before = false, ---@type boolean|number[]
-          -- position of the label extmark
-          style = "overlay", ---@type "eol" | "overlay" | "right_align" | "inline"
-          -- flash tries to re-use labels that were already assigned to a position,
-          -- when typing more characters. By default only lower-case labels are re-used.
-          reuse = "lowercase", ---@type "lowercase" | "all"
-        },
         -- show a backdrop with hl FlashBackdrop
         backdrop = false,
         -- Highlight the search matches
