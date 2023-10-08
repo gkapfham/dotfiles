d1d76bec94e809131a2ada0728a21c78eeb938ed
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2023-10-08 12:03:07 -0400

    Reconfigure the keymaps for tree-climber.nvim in treesitter.lua.

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -28,14 +28,14 @@ return {
     event = "VeryLazy",
     config = function()
       local keyopts = { noremap = true, silent = true }
-      vim.keymap.set({'n', 'v', 'o'}, '<c-h>', require('tree-climber').goto_parent, keyopts)
-      vim.keymap.set({'n', 'v', 'o'}, '<c-l>', require('tree-climber').goto_child, keyopts)
-      vim.keymap.set({'n', 'v', 'o'}, '<c-j>', require('tree-climber').goto_next, keyopts)
-      vim.keymap.set({'n', 'v', 'o'}, '<c-k>', require('tree-climber').goto_prev, keyopts)
+      vim.keymap.set({'n', 'v', 'o'}, 'H', require('tree-climber').goto_parent, keyopts)
+      vim.keymap.set({'n', 'v', 'o'}, 'L', require('tree-climber').goto_child, keyopts)
+      vim.keymap.set({'n', 'v', 'o'}, 'J', require('tree-climber').goto_next, keyopts)
+      vim.keymap.set({'n', 'v', 'o'}, 'K', require('tree-climber').goto_prev, keyopts)
       vim.keymap.set({'v', 'o'}, 'in', require('tree-climber').select_node, keyopts)
-      -- vim.keymap.set('n', '<c-k>', require('tree-climber').swap_prev, keyopts)
-      -- vim.keymap.set('n', '<c-j>', require('tree-climber').swap_next, keyopts)
-      -- vim.keymap.set('n', '<c-h>', require('tree-climber').highlight_node, keyopts)
+      vim.keymap.set('n', '<c-k>', require('tree-climber').swap_prev, keyopts)
+      vim.keymap.set('n', '<c-j>', require('tree-climber').swap_next, keyopts)
+      vim.keymap.set('n', '<c-h>', require('tree-climber').highlight_node, keyopts)
     end
   },
 
