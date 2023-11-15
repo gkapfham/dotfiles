dcdeabc95bdd86e22a9862412f77ae0628ceea10
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2023-11-15 12:23:44 -0500

    Add use of a different treesitter navigation plugin in treesitter.lua.

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -22,22 +22,58 @@ return {
     event = "VeryLazy"
   },
 
-  -- Tree-climber
+  -- Treesitter movements
+
   {
-    "drybalka/tree-climber.nvim",
+    'gsuuon/tshjkl.nvim',
     event = "VeryLazy",
-    config = function()
-      local keyopts = { noremap = true, silent = true }
-      vim.keymap.set({'n', 'v', 'o'}, 'H', require('tree-climber').goto_parent, keyopts)
-      vim.keymap.set({'n', 'v', 'o'}, 'L', require('tree-climber').goto_child, keyopts)
-      vim.keymap.set({'n', 'v', 'o'}, 'J', require('tree-climber').goto_next, keyopts)
-      vim.keymap.set({'n', 'v', 'o'}, 'K', require('tree-climber').goto_prev, keyopts)
-      vim.keymap.set({'v', 'o'}, 'in', require('tree-climber').select_node, keyopts)
-      vim.keymap.set('n', '<c-k>', require('tree-climber').swap_prev, keyopts)
-      vim.keymap.set('n', '<c-j>', require('tree-climber').swap_next, keyopts)
-      vim.keymap.set('n', '<c-h>', require('tree-climber').highlight_node, keyopts)
-    end
+    opts = {
+      keymaps = {
+        toggle = '<Space>tn',
+      },
+      marks = {
+        parent = {
+          virt_text = { {'h', 'ModeMsg'} },
+          virt_text_pos = 'overlay'
+        },
+        child = {
+          virt_text = { {'l', 'ModeMsg'} },
+          virt_text_pos = 'overlay'
+        },
+        prev = {
+          virt_text = { {'k', 'ModeMsg'} },
+          virt_text_pos = 'overlay'
+        },
+        next = {
+          virt_text = { {'j', 'ModeMsg'} },
+          virt_text_pos = 'overlay'
+        }
+      }
+    }
   },
+
+  -- {
+  --   'gsuuon/tshjkl.nvim',
+  --   event = "VeryLazy",
+  --   config = true
+  -- },
+
+  -- -- Tree-climber
+  -- {
+  --   "drybalka/tree-climber.nvim",
+  --   event = "VeryLazy",
+  --   config = function()
+  --     local keyopts = { noremap = true, silent = true }
+  --     vim.keymap.set({'n', 'v', 'o'}, 'H', require('tree-climber').goto_parent, keyopts)
+  --     vim.keymap.set({'n', 'v', 'o'}, 'L', require('tree-climber').goto_child, keyopts)
+  --     vim.keymap.set({'n', 'v', 'o'}, 'J', require('tree-climber').goto_next, keyopts)
+  --     vim.keymap.set({'n', 'v', 'o'}, 'K', require('tree-climber').goto_prev, keyopts)
+  --     vim.keymap.set({'v', 'o'}, 'in', require('tree-climber').select_node, keyopts)
+  --     vim.keymap.set('n', '<c-k>', require('tree-climber').swap_prev, keyopts)
+  --     vim.keymap.set('n', '<c-j>', require('tree-climber').swap_next, keyopts)
+  --     vim.keymap.set('n', '<c-h>', require('tree-climber').highlight_node, keyopts)
+  --   end
+  -- },
 
   -- -- Improve indentation that addresses
   -- -- the limitation of treesitter for Python
