9f8dff45e31cd1d03b98a3877f97c77872f54920
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2023-11-15 13:15:55 -0500

    Delete the nvim-yati and the treeclimber plugins in treesitter.lua.

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -52,30 +52,6 @@ return {
     }
   },
 
-  -- -- Tree-climber
-  -- {
-  --   "drybalka/tree-climber.nvim",
-  --   event = "VeryLazy",
-  --   config = function()
-  --     local keyopts = { noremap = true, silent = true }
-  --     vim.keymap.set({'n', 'v', 'o'}, 'H', require('tree-climber').goto_parent, keyopts)
-  --     vim.keymap.set({'n', 'v', 'o'}, 'L', require('tree-climber').goto_child, keyopts)
-  --     vim.keymap.set({'n', 'v', 'o'}, 'J', require('tree-climber').goto_next, keyopts)
-  --     vim.keymap.set({'n', 'v', 'o'}, 'K', require('tree-climber').goto_prev, keyopts)
-  --     vim.keymap.set({'v', 'o'}, 'in', require('tree-climber').select_node, keyopts)
-  --     vim.keymap.set('n', '<c-k>', require('tree-climber').swap_prev, keyopts)
-  --     vim.keymap.set('n', '<c-j>', require('tree-climber').swap_next, keyopts)
-  --     vim.keymap.set('n', '<c-h>', require('tree-climber').highlight_node, keyopts)
-  --   end
-  -- },
-
-  -- -- Improve indentation that addresses
-  -- -- the limitation of treesitter for Python
-  -- {
-  --   "yioneko/nvim-yati",
-  --   event = "VeryLazy"
-  -- },
-
   -- Treesitter
   {
     "nvim-treesitter/nvim-treesitter",
