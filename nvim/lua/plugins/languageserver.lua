5efff7dba7e9d9be0b024d3be1d4aeb58b61bf11
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2023-11-15 12:44:01 -0500

    Add the plugin for displaying symbol definition and use in languageserver.lua.

--- nvim/lua/plugins/languageserver.lua
+++ nvim/lua/plugins/languageserver.lua
@@ -2,6 +2,53 @@
 -- Purpose: load and configure plugins for installation
 -- and use of language servers protocol implementations
 
+local function h(name) return vim.api.nvim_get_hl(0, { name = name }) end
+
+-- hl-groups can have any name
+vim.api.nvim_set_hl(0, 'SymbolUsageRounding', { fg = h('CursorLine').bg, italic = true })
+vim.api.nvim_set_hl(0, 'SymbolUsageContent', { bg = h('CursorLine').bg, fg = h('Comment').fg, italic = true })
+vim.api.nvim_set_hl(0, 'SymbolUsageRef', { fg = h('Function').fg, bg = h('CursorLine').bg, italic = true })
+vim.api.nvim_set_hl(0, 'SymbolUsageDef', { fg = h('Type').fg, bg = h('CursorLine').bg, italic = true })
+vim.api.nvim_set_hl(0, 'SymbolUsageImpl', { fg = h('@keyword').fg, bg = h('CursorLine').bg, italic = true })
+
+local function text_format(symbol)
+  local res = {}
+
+  local round_start = { '', 'SymbolUsageRounding' }
+  local round_end = { '', 'SymbolUsageRounding' }
+
+  if symbol.references then
+    local usage = symbol.references <= 1 and 'usage' or 'usages'
+    local num = symbol.references == 0 and 'no' or symbol.references
+    table.insert(res, round_start)
+    table.insert(res, { '󰌹 ', 'SymbolUsageRef' })
+    table.insert(res, { ('%s %s'):format(num, usage), 'SymbolUsageContent' })
+    table.insert(res, round_end)
+  end
+
+  if symbol.definition then
+    if #res > 0 then
+      table.insert(res, { ' ', 'NonText' })
+    end
+    table.insert(res, round_start)
+    table.insert(res, { '󰳽 ', 'SymbolUsageDef' })
+    table.insert(res, { symbol.definition .. ' defs', 'SymbolUsageContent' })
+    table.insert(res, round_end)
+  end
+
+  if symbol.implementation then
+    if #res > 0 then
+      table.insert(res, { ' ', 'NonText' })
+    end
+    table.insert(res, round_start)
+    table.insert(res, { '󰡱 ', 'SymbolUsageImpl' })
+    table.insert(res, { symbol.implementation .. ' impls', 'SymbolUsageContent' })
+    table.insert(res, round_end)
+  end
+
+  return res
+end
+
 return {
 
   -- Mason.nvim for LSP management
@@ -189,6 +236,16 @@ return {
       -- Perform a format of the content in the buffer
       { "<Space>ff", "<cmd> lua vim.lsp.buf.format()<CR>", desc = "Language Server: format buffer" },
     }
+  },
+
+  {
+    'Wansmer/symbol-usage.nvim',
+    event = 'BufReadPre',
+    config = function()
+      require('symbol-usage').setup({
+        text_format = text_format,
+      })
+    end
   },
 
   -- venv-selector.nvim for selecting virtual environments after starting neovim
