715680e20b7c161ec7b419006794698bc82fa933
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2023-11-22 13:13:50 -0500

    Delete a commented-out section in autocmds.lua for returning to a location in a buffer.

--- nvim/lua/configure/autocmds.lua
+++ nvim/lua/configure/autocmds.lua
@@ -11,17 +11,6 @@ vim.api.nvim_create_autocmd("TextYankPost", {
   end,
 })
 
--- Go to the last location when opening a buffer
--- vim.api.nvim_create_autocmd("BufReadPost", {
---   callback = function()
---     local mark = vim.api.nvim_buf_get_mark(0, '"')
---     local lcount = vim.api.nvim_buf_line_count(0)
---     if mark[1] > 0 and mark[1] <= lcount then
---       pcall(vim.api.nvim_win_set_cursor, 0, mark)
---     end
---   end,
--- })
-
 -- Close some filetypes with <q>
 vim.api.nvim_create_autocmd({ "FileType" }, {
   pattern = {
