f7407b99c8a97e1c7fb33d787c0ad1141f118fcf
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2023-11-24 21:19:28 -0500

    Fix formatting and improve comments in the quarto.lua file.

--- nvim/lua/plugins/quarto.lua
+++ nvim/lua/plugins/quarto.lua
@@ -5,40 +5,41 @@
 
 return {
 
-  {
-    'quarto-dev/quarto-nvim',
-    event = "BufReadPost",
-    dependencies = {
-        { 'hrsh7th/nvim-cmp'  },
-        { 'jmbuhr/otter.nvim' },
-    },
-    config = function()
-        require('quarto').setup {
-            closePreviewOnExit = true,
-            lspFeatures = {
-                enabled = true,
-                languages = { 'r', 'python', 'bash' },
-                chunks = 'curly',
-                diagnostics = {
+    -- quarto-nvim and otter.nvim
+    {
+        'quarto-dev/quarto-nvim',
+        event = "BufReadPost",
+        dependencies = {
+            { 'hrsh7th/nvim-cmp' },
+            { 'jmbuhr/otter.nvim' },
+        },
+        config = function()
+            require('quarto').setup {
+                closePreviewOnExit = true,
+                lspFeatures = {
                     enabled = true,
-                    triggers = { "BufWritePost" }
-                },
-                completion = {
-                    enabled = true
+                    languages = { 'r', 'python', 'bash' },
+                    chunks = 'curly',
+                    diagnostics = {
+                        enabled = true,
+                        triggers = { "BufWritePost" }
+                    },
+                    completion = {
+                        enabled = true
+                    }
                 }
             }
-        }
-        -- Open Quarto Preview in a buffer instead of a tab
-        vim.cmd [[
+            -- open Quarto Preview in a buffer instead of a tab
+            vim.cmd [[
             function! QuartoPreview()
-                :w!
-                :terminal quarto preview %:p --to html
-                :call feedkeys('<Esc>')
-                :bprev
+            :w!
+            :terminal quarto preview %:p --to html
+            :call feedkeys('<Esc>')
+            :bprev
             endfunction
-        ]]
-        vim.keymap.set('n', '<localleader>P', ':call QuartoPreview() <CR>')
-    end
-  },
+            ]]
+            vim.keymap.set('n', '<localleader>P', ':call QuartoPreview() <CR>')
+        end
+    },
 
 }
