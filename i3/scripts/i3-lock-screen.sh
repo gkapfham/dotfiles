445ab83c91504b9447d69a896901d96c01e786ac
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2023-12-18 23:37:27 -0500

    Use a simpler lock screen that is also faster in i3-lock-screen.sh.

--- i3/scripts/i3-lock-screen.sh
+++ i3/scripts/i3-lock-screen.sh
@@ -1,5 +1,5 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-# New version of the command using more dash separators
-
-i3lock --indicator -e --greeter-font='Hack Nerd Font' --wrong-font='Hack Nerd Font' --verif-font='Hack Nerd Font' --lockfailed-text='Failed' --greeter-text='' --greeter-pos='750:700' --ring-width=15 --greeter-size=75 --greeter-color=875f87ff --wrong-text='Incorrect' --verif-text='Verifying' --lock-text='Locking' --verif-size=45 --wrong-size=45 --radius=150 --inside-color=875f87ff --ringver-color=5f8700ff --keyhl-color=b5bd68ff --line-color=875f87ff --ringwrong-color=5f8700ff --insidever-color=81a2beff --insidewrong-color=d75f5fff --line-uses-inside --separator-color=5f8700ff -c 1c1c1c
+# Simple and fast lockscreen that blurs current window
+sleep 0.1
+i3lock-fancy-rapid 5 3 -u
