8f56ab503e921edca50ecec26881efada77907ca
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2023-12-21 22:17:11 -0500

    Improve comment in the i3-lock-screen.sh script to explain the delay.

--- i3/scripts/i3-lock-screen.sh
+++ i3/scripts/i3-lock-screen.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
-# Simple and fast lockscreen that blurs current window
+# Simple and fast lockscreen that blurs current window;
+# note that there is a short delay before the screen is
+# locked, so you can see the current window blurred and
+# not actually see the rofi menu that ran this script
 sleep 0.1
 i3lock-fancy-rapid 5 3 -u
