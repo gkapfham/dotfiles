005cde461b5a5eb738f1b8aa55558473ba4c77d4
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2024-01-08 12:46:16 -0500

    Add a comment about the old has_words_before function in completion.lua.

--- nvim/lua/plugins/completion.lua
+++ nvim/lua/plugins/completion.lua
@@ -52,6 +52,7 @@ local kind_icons = {
 -- that this function must exist for other code in
 -- this file to work correctly
 
+-- Old version of the function before using GitHub copilot
 -- local has_words_before = function()
 --   local line, col = unpack(vim.api.nvim_win_get_cursor(0))
 --   return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
