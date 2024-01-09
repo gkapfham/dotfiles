9ba50b491bf578c12a514ea7e5f2fe6331f0ea9a
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2024-01-09 12:04:09 -0500

    Revise heavily the autopairs.lua plugin and start using nvim-autpairs.

--- nvim/lua/plugins/autopairs.lua
+++ nvim/lua/plugins/autopairs.lua
@@ -3,13 +3,20 @@
 
 return {
 
-  -- -- ultimate-autopairs.nvim
-  -- {
-  --   'altermo/ultimate-autopair.nvim',
-  --   event={'InsertEnter','CmdlineEnter'},
-  --   opts={
-  --   },
-  -- },
+  -- nvim-autpairs: automatically create pairs
+  -- configuration ensures that when you have
+  -- variable = |value
+  -- and you press ( it does not add ()
+  -- (note that | represents the cursor)
+  {
+    'windwp/nvim-autopairs',
+    event={'InsertEnter','CmdlineEnter'},
+    config=function()
+      require('nvim-autopairs').setup({
+        ignored_next_char = "[%w%.]"
+      })
+    end,
+  },
 
   -- nvim-ts-autotag: automatically generate tags
   -- for html/xml paired tags, like <div></div>
