cf4d70daa1a576eaff3a538f1e89d1a11a7b1b5b
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2024-02-08 17:00:36 -0500

    Stop using the ast_grep program in the telescope.lua file.

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -121,7 +121,7 @@ return {
       -- Load and configure the advanced_git_search plugin
       require("telescope").load_extension("advanced_git_search")
       -- Load and configure the ast_grep
-      require("telescope").load_extension("ast_grep")
+      -- require("telescope").load_extension("ast_grep")
       -- Configure the keymap for refactoring; setting it here because
       -- I don't know how to set visual mode keymaps in keys section of spec
       vim.api.nvim_set_keymap(
@@ -136,7 +136,7 @@ return {
       -- Aerial
       { "<Space>ta", "<cmd> Telescope aerial <CR>",                    desc = "Telescope: Aerial" },
       -- Ast-Grep
-      { "<Space>tg", "<cmd> Telescope ast_grep <CR>",                  desc = "Telescope: AST grep" },
+      -- { "<Space>tg", "<cmd> Telescope ast_grep <CR>",                  desc = "Telescope: AST grep" },
       -- Buffers
       { "<Space>i",  "<cmd> Telescope buffers <CR>",                   desc = "Telescope: Buffers" },
       { "<Space>tf", "<cmd> Telescope current_buffer_fuzzy_find <CR>", desc = "Telescope: Fuzzy search buffers" },
