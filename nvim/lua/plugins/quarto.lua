290c23bd6dfb7b384494d8da240dd136b2b551dd
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2024-02-10 17:36:57 -0500

    Use more consistent name of function as config in the quarto.lua file.

--- nvim/lua/plugins/quarto.lua
+++ nvim/lua/plugins/quarto.lua
@@ -5,13 +5,13 @@
 
 return {
 
+    -- molten-nvim
     {
         "benlubas/molten-nvim",
         event = "VeryLazy",
         version = "^1.0.0", -- use version <2.0.0 to avoid breaking changes
         build = ":UpdateRemotePlugins",
-        init = function()
-            -- this is an example, not a default. Please see the readme for more configuration options
+        config = function()
             vim.g.molten_output_win_max_height = 12
             vim.keymap.set("n", "<localleader>e", ":MoltenEvaluateOperator<CR>",
                 { desc = "evaluate operator", silent = true })
