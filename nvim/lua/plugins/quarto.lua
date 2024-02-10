b9760840fa1271060d014671636470ff01b9847e
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2024-02-10 17:32:53 -0500

    Perform a major overhaul of the quarto.lua file to get code execution working inside of a .qmd file! Hooray!

--- nvim/lua/plugins/quarto.lua
+++ nvim/lua/plugins/quarto.lua
@@ -5,6 +5,23 @@
 
 return {
 
+    {
+        "benlubas/molten-nvim",
+        event = "VeryLazy",
+        version = "^1.0.0", -- use version <2.0.0 to avoid breaking changes
+        build = ":UpdateRemotePlugins",
+        init = function()
+            -- this is an example, not a default. Please see the readme for more configuration options
+            vim.g.molten_output_win_max_height = 12
+            vim.keymap.set("n", "<localleader>e", ":MoltenEvaluateOperator<CR>",
+                { desc = "evaluate operator", silent = true })
+            vim.keymap.set("n", "<localleader>os", ":noautocmd MoltenEnterOutput<CR>",
+                { desc = "open output window", silent = true })
+            vim.g.molten_virt_text_output = true
+            vim.g.molten_virt_lines_off_by_1 = true
+        end,
+    },
+
     -- quarto-nvim and otter.nvim
     {
         'quarto-dev/quarto-nvim',
@@ -26,9 +43,22 @@ return {
                     },
                     completion = {
                         enabled = true
-                    }
-                }
+                    },
+                },
+                codeRunner = {
+                    enabled = true,
+                    default_method = "molten",
+                },
             }
+            local runner = require("quarto.runner")
+            vim.keymap.set("n", "<localleader>rc", runner.run_cell, { desc = "run cell", silent = true })
+            vim.keymap.set("n", "<localleader>ra", runner.run_above, { desc = "run cell and above", silent = true })
+            vim.keymap.set("n", "<localleader>rA", runner.run_all, { desc = "run all cells", silent = true })
+            vim.keymap.set("n", "<localleader>rl", runner.run_line, { desc = "run line", silent = true })
+            vim.keymap.set("v", "<localleader>r", runner.run_range, { desc = "run visual range", silent = true })
+            vim.keymap.set("n", "<localleader>RA", function()
+                runner.run_all(true)
+            end, { desc = "run all cells of all languages", silent = true })
             -- open Quarto Preview in a buffer instead of a tab
             vim.cmd [[
             function! QuartoPreview()
