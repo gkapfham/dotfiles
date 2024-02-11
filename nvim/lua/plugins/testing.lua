c14b875f203f8e9688a38d8aabc6c0bb33db60ed
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2024-02-10 20:27:54 -0500

    Delete the commented-out version in the testing.lua file.

--- nvim/lua/plugins/testing.lua
+++ nvim/lua/plugins/testing.lua
@@ -53,36 +53,4 @@ return {
     end
   },
 
-  -- -- Test
-  -- {
-  --   "nvim-neotest/neotest-python",
-  --   dependencies = {
-  --     "nvim-neotest/neotest",
-  --     "nvim-treesitter/nvim-treesitter",
-  --   },
-  --   event = "VeryLazy",
-  --   config = function()
-  --     require("neotest").setup({
-  --       adapters = {
-  --         require("neotest-python")
-  --       },
-  --       quickfix = {
-  --         enabled = true,
-  --         open = true
-  --       },
-  --     })
-  --     vim.cmd([[
-  --       " au Filetype neotest-output-panel setlocal listchars+=tab:\ \
-  --       command! TestSummary lua require("neotest").summary.toggle()
-  --       command! TestFile lua require("neotest").run.run(vim.fn.expand("%"))
-  --       command! Test lua require("neotest").run.run(vim.fn.getcwd())
-  --       command! TestNearest lua require("neotest").run.run()
-  --       command! TestDebug lua require("neotest").run.run({ strategy = "dap" })
-  --       command! TestAttach lua require("neotest").run.attach()
-  --       command! TestOutput lua require("neotest").output.toggle()
-  --       command! TestRunner lua require("neotest").output_panel.toggle()
-  --     ]])
-  --   end,
-  -- },
-
 }
