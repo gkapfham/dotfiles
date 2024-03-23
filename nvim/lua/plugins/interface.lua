e4b0e77d339b9bf1dc7285c3ef03d429378fb5ba
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2024-03-23 01:34:02 -0400

    Revert to the use of height = 0.5 for neo-tree in nvim/lua/plugins/interface.lua.

--- nvim/lua/plugins/interface.lua
+++ nvim/lua/plugins/interface.lua
@@ -81,7 +81,7 @@ return {
           filter = function(buf)
             return vim.b[buf].neo_tree_source == "filesystem"
           end,
-          -- size = { height = 0.5 },
+          size = { height = 0.5 },
         },
         -- Neotree Buffers
         {
