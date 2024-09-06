bfdd28290104ed469dca693e53305a6267da3415
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2024-09-06 08:34:52 -0400

    chore: Remove the symbol-usage.nvim plugin in nvim/lua/plugins/languageserver.lua as it stopped working.

--- nvim/lua/plugins/languageserver.lua
+++ nvim/lua/plugins/languageserver.lua
@@ -238,48 +238,48 @@ return {
     }
   },
 
-  -- symbol-usage.nvim displays symbol usage information in virtual text
-  {
-    'Wansmer/symbol-usage.nvim',
-    event = "BufReadPre",
-    config = function()
-      local function h(name) return vim.api.nvim_get_hl(0, { name = name }) end
-      local function text_format(symbol)
-        local res = {}
-        local round_start = { '', 'SymbolUsageRounding' }
-        local round_end = { '', 'SymbolUsageRounding' }
-        if symbol.references then
-          local usage = symbol.references <= 1 and 'usage' or 'usages'
-          local num = symbol.references == 0 and 'no' or symbol.references
-          table.insert(res, round_start)
-          table.insert(res, { ' ', 'SymbolUsageRef' })
-          table.insert(res, { ('%s %s'):format(num, usage), 'SymbolUsageContent' })
-          table.insert(res, round_end)
-        end
-        if symbol.definition then
-          if #res > 0 then
-            table.insert(res, { ' ', 'NonText' })
-          end
-          table.insert(res, round_start)
-          table.insert(res, { '󰳽 ', 'SymbolUsageDef' })
-          table.insert(res, { symbol.definition .. ' defs', 'SymbolUsageContent' })
-          table.insert(res, round_end)
-        end
-        if symbol.implementation then
-          if #res > 0 then
-            table.insert(res, { ' ', 'NonText' })
-          end
-          table.insert(res, round_start)
-          table.insert(res, { '󰡱 ', 'SymbolUsageImpl' })
-          table.insert(res, { symbol.implementation .. ' impls', 'SymbolUsageContent' })
-          table.insert(res, round_end)
-        end
-        return res
-      end
-      require('symbol-usage').setup({
-        text_format = text_format,
-      })
-    end
-  },
+  -- -- symbol-usage.nvim displays symbol usage information in virtual text
+  -- {
+  --   'Wansmer/symbol-usage.nvim',
+  --   event = "BufReadPre",
+  --   config = function()
+  --     local function h(name) return vim.api.nvim_get_hl(0, { name = name }) end
+  --     local function text_format(symbol)
+  --       local res = {}
+  --       local round_start = { '', 'SymbolUsageRounding' }
+  --       local round_end = { '', 'SymbolUsageRounding' }
+  --       if symbol.references then
+  --         local usage = symbol.references <= 1 and 'usage' or 'usages'
+  --         local num = symbol.references == 0 and 'no' or symbol.references
+  --         table.insert(res, round_start)
+  --         table.insert(res, { ' ', 'SymbolUsageRef' })
+  --         table.insert(res, { ('%s %s'):format(num, usage), 'SymbolUsageContent' })
+  --         table.insert(res, round_end)
+  --       end
+  --       if symbol.definition then
+  --         if #res > 0 then
+  --           table.insert(res, { ' ', 'NonText' })
+  --         end
+  --         table.insert(res, round_start)
+  --         table.insert(res, { '󰳽 ', 'SymbolUsageDef' })
+  --         table.insert(res, { symbol.definition .. ' defs', 'SymbolUsageContent' })
+  --         table.insert(res, round_end)
+  --       end
+  --       if symbol.implementation then
+  --         if #res > 0 then
+  --           table.insert(res, { ' ', 'NonText' })
+  --         end
+  --         table.insert(res, round_start)
+  --         table.insert(res, { '󰡱 ', 'SymbolUsageImpl' })
+  --         table.insert(res, { symbol.implementation .. ' impls', 'SymbolUsageContent' })
+  --         table.insert(res, round_end)
+  --       end
+  --       return res
+  --     end
+  --     require('symbol-usage').setup({
+  --       text_format = text_format,
+  --     })
+  --   end
+  -- },
 
 }
