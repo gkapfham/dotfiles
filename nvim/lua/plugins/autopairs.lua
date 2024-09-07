50f279c6d71c0d5f8ae38df744d1200ea95afcf0
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2024-09-07 13:36:45 -0400

    Stop using the autopairs plugin and add mini.pairs instead in nvim/lua/plugins/autopairs.lua.

--- nvim/lua/plugins/autopairs.lua
+++ nvim/lua/plugins/autopairs.lua
@@ -3,17 +3,49 @@
 
 return {
 
-  -- nvim-autopairs: automatically create pairs
-  -- configuration ensures that when you have
-  -- variable = |value
-  -- and you press ( it does not add ()
-  -- (note that | represents the cursor)
+  -- -- nvim-autopairs: automatically create pairs
+  -- -- configuration ensures that when you have
+  -- -- variable = |value
+  -- -- and you press ( it does not add ()
+  -- -- (note that | represents the cursor)
+  -- {
+  --   'windwp/nvim-autopairs',
+  --   event={'InsertEnter','CmdlineEnter'},
+  --   config=function()
+  --     require('nvim-autopairs').setup({
+  --       ignored_next_char = "[%w%.]"
+  --     })
+  --   end,
+  -- },
+
+  -- mini.pairs
+  -- Pairs management
   {
-    'windwp/nvim-autopairs',
-    event={'InsertEnter','CmdlineEnter'},
-    config=function()
-      require('nvim-autopairs').setup({
-        ignored_next_char = "[%w%.]"
+    "echasnovski/mini.pairs",
+    event = "VeryLazy",
+    config = function()
+      -- Use gz mappings instead of s to prevent conflict with flash.nvim
+      require("mini.pairs").setup({
+        -- In which modes mappings from this `config` should be created
+        modes = { insert = true, command = false, terminal = false },
+        -- Global mappings. Each right hand side should be a pair information, a
+        -- table with at least these fields (see more in |MiniPairs.map|):
+        -- - <action> - one of 'open', 'close', 'closeopen'.
+        -- - <pair> - two character string for pair to be used.
+        -- By default pair is not inserted after `\`, quotes are not recognized by
+        -- `<CR>`, `'` does not insert pair after a letter.
+        -- Only parts of tables can be tweaked (others will use these defaults).
+        mappings = {
+          ['('] = { action = 'open', pair = '()', neigh_pattern = '[^\\].' },
+          ['['] = { action = 'open', pair = '[]', neigh_pattern = '[^\\].' },
+          ['{'] = { action = 'open', pair = '{}', neigh_pattern = '[^\\].' },
+          [')'] = { action = 'close', pair = '()', neigh_pattern = '[^\\].' },
+          [']'] = { action = 'close', pair = '[]', neigh_pattern = '[^\\].' },
+          ['}'] = { action = 'close', pair = '{}', neigh_pattern = '[^\\].' },
+          ['"'] = { action = 'closeopen', pair = '""', neigh_pattern = '[^\\].', register = { cr = false } },
+          ["'"] = { action = 'closeopen', pair = "''", neigh_pattern = '[^%a\\].', register = { cr = false } },
+          ['`'] = { action = 'close', pair = '``', neigh_pattern = '[^\\].', register = { cr = false } },
+        },
       })
     end,
   },
