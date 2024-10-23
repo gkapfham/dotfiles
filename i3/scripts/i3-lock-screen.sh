4e0c2f6ad9a17832318ec0be2df1ce3e2cbcd5a6
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2024-10-23 14:09:34 -0400

    Increase the sleep time to handle animations better in i3/scripts/i3-lock-screen.sh.

--- i3/scripts/i3-lock-screen.sh
+++ i3/scripts/i3-lock-screen.sh
@@ -4,5 +4,5 @@
 # note that there is a short delay before the screen is
 # locked, so you can see the current window blurred and
 # not actually see the rofi menu that ran this script
-sleep 0.1
+sleep 0.25
 i3lock-fancy-rapid 5 3 -u
