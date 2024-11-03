9d6f2f98aef9385734f953f819030f3178a99b81
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2024-11-03 16:57:03 -0500

    Add new keybindings to the nvim/lua/plugins/zk.lua file for zk.

--- nvim/lua/plugins/zk.lua
+++ nvim/lua/plugins/zk.lua
@@ -25,10 +25,20 @@ return {
       -- Add the key mappings only for Markdown files in a zk notebook
       if require("zk.util").notebook_root(vim.fn.expand('%:p')) ~= nil then
         local function map(...) vim.api.nvim_buf_set_keymap(0, ...) end
-        local opts = { noremap=true, silent=false }
+        local opts = { noremap = true, silent = false }
         -- Preview a linked note
         map("n", "K", "<Cmd>lua vim.lsp.buf.hover()<CR>", opts)
       end
+      local zk = require("zk")
+      local commands = require("zk.commands")
+      local function make_edit_fn(defaults, picker_options)
+        return function(options)
+          options = vim.tbl_extend("force", defaults, options or {})
+          zk.edit(options, picker_options)
+        end
+      end
+      commands.add("ZkOrphans", make_edit_fn({ orphan = true }, { title = "Zk Orphans" }))
+      commands.add("ZkRecents", make_edit_fn({ createdAfter = "2 weeks ago" }, { title = "Zk Recents" }))
     end,
     -- Keys
     keys = {
@@ -36,6 +46,8 @@ return {
       { "<Space>zn", "<Cmd>ZkNew { title = vim.fn.input('Title: ') } <CR>", desc = "Zk: New Note" },
       -- Open notes
       { "<Space>zo", "<Cmd>ZkNotes { sort = { 'modified' } } <CR>",         desc = "Zk: Open Notes" },
+      -- Open recent notes
+      { "<Space>zr", "<Cmd>ZkRecents <CR>",                                 desc = "Zk: Open Recent Notes" },
       -- Open notes by tags
       { "<Space>zt", "<Cmd>ZkTags <CR>",                                    desc = "Zk: Open Notes by Tags" },
       -- Open backlinks to a note
