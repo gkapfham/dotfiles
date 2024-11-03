6a9aab7b5c32ef1b7bea17bd2ac231e8d721a729
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2024-11-02 21:30:31 -0400

    Add more key mappings for zk in the nvim/lua/plugins/zk.lua file.

--- nvim/lua/plugins/zk.lua
+++ nvim/lua/plugins/zk.lua
@@ -5,7 +5,7 @@
 
 return {
 
-  { 
+  {
     "zk-org/zk-nvim",
     event = "VeryLazy",
     config = function()
@@ -22,7 +22,23 @@ return {
           },
         },
       })
-    end
+    end,
+    -- Keys
+    keys = {
+      -- New note
+      { "<Space>zn", "<Cmd>ZkNew { title = vim.fn.input('Title: ') } <CR>", desc = "Zk: New Note" },
+      -- Open notes
+      { "<Space>zo", "<Cmd>ZkNotes { sort = { 'modified' } } <CR>", desc = "Zk: Open Notes" },
+      -- Open notes by tags
+      { "<Space>zt", "<Cmd>ZkTags <CR>", desc = "Zk: Open Notes by Tags" },
+      -- Open backlinks to a note
+      { "<Space>zt", "<Cmd>ZkBacklinks <CR>", desc = "Zk: Open Backlinks to a Note" },
+      -- Find notes
+      { "<Space>zf", "<Cmd>ZkNotes { sort = { 'modified' }, match = { vim.fn.input('Search: ') } } <CR>",
+        desc = "Zk: Find Matching Notes" },
+      -- Find notes in visual mode
+      { "<Space>zf", ":'<,'>ZkMatch<CR>", desc = "Zk: Find Matching Notes", mode = "v" },
+    },
   }
 
 }
