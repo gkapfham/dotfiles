987a6cfab5d9c1d42784aea1364b00380638630f
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2024-11-04 15:16:54 -0500

    Significantly update the nvim/lua/plugins/languageserver.lua to use old approach for symbol references.

--- nvim/lua/plugins/languageserver.lua
+++ nvim/lua/plugins/languageserver.lua
@@ -277,17 +277,14 @@ return {
       end
       local function text_format(symbol)
         local res = {}
-
         -- Indicator that shows if there are any other symbols in the same line
         local stacked_functions_content = symbol.stacked_count > 0 and ("+%s"):format(symbol.stacked_count) or ""
-
         if symbol.references then
           local usage = symbol.references <= 1 and "usage" or "usages"
           local num = symbol.references == 0 and "no" or symbol.references
           table.insert(res, { "󰌹 ", "SymbolUsageRef" })
           table.insert(res, { ("%s %s"):format(num, usage), "SymbolUsageContent" })
         end
-
         if symbol.definition then
           if #res > 0 then
             table.insert(res, { " ", "NonText" })
@@ -295,7 +292,6 @@ return {
           table.insert(res, { "󰳽 ", "SymbolUsageDef" })
           table.insert(res, { symbol.definition .. " defs", "SymbolUsageContent" })
         end
-
         if symbol.implementation then
           if #res > 0 then
             table.insert(res, { " ", "NonText" })
@@ -303,7 +299,6 @@ return {
           table.insert(res, { "󰡱 ", "SymbolUsageImpl" })
           table.insert(res, { symbol.implementation .. " impls", "SymbolUsageContent" })
         end
-
         if stacked_functions_content ~= "" then
           if #res > 0 then
             table.insert(res, { " ", "NonText" })
@@ -311,7 +306,6 @@ return {
           table.insert(res, { " ", "SymbolUsageImpl" })
           table.insert(res, { stacked_functions_content, "SymbolUsageContent" })
         end
-
         return res
       end
       require("symbol-usage").setup({
@@ -337,7 +331,6 @@ return {
       })
     end
   }
-
 
   -- -- symbol-usage.nvim displays symbol usage information in virtual text
   -- {
