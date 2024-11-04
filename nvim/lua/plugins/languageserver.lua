3a02258d65e9b5528295ba7129bc41d95095ed4b
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2024-11-03 21:21:22 -0500

    Try a new approach to symbol usage that displays more details at the end of line in nvim/lua/plugins/languageserver.lua.

--- nvim/lua/plugins/languageserver.lua
+++ nvim/lua/plugins/languageserver.lua
@@ -238,68 +238,130 @@ return {
     }
   },
 
-  -- lsp-lens.nvim for symbol usage information in virtual text
+  -- -- lsp-lens.nvim for symbol usage information in virtual text
+  -- {
+  --   "VidocqH/lsp-lens.nvim",
+  --   event = "BufReadPre",
+  --   config = function()
+  --     require 'lsp-lens'.setup({
+  --       enable = true,
+  --       include_declaration = true,
+  --       separator = ", ",
+  --       sections = {
+  --         definition = function(count)
+  --           return "󰐖 Definitions " .. count
+  --         end,
+  --         references = function(count)
+  --           return " References " .. count - 1
+  --         end,
+  --         implements = function(count)
+  --           return " Implements " .. count
+  --         end,
+  --         git_authors = function(latest_author, count)
+  --           return " " .. latest_author .. (count - 1 == 0 and "" or (" + " .. count - 1))
+  --         end,
+  --       },
+  --     })
+  --   end
+  -- },
+
+  -- symbol-usage.nvim displays symbol usage information in virtual text
   {
-    "VidocqH/lsp-lens.nvim",
-    event = "BufReadPre",
+    'Wansmer/symbol-usage.nvim',
+    -- event = "BufReadPre",
+    event = "LspAttach",
     config = function()
-      require 'lsp-lens'.setup({
-        enable = true,
-        include_declaration = true,
-        separator = ", ",
-        sections = {
-          definition = function(count)
-            return "󰐖 Definitions " .. count
-          end,
-          references = function(count)
-            return " References " .. count - 1
-          end,
-          implements = function(count)
-            return " Implements " .. count
-          end,
-          git_authors = function(latest_author, count)
-            return " " .. latest_author .. (count - 1 == 0 and "" or (" + " .. count - 1))
-          end,
+      local SymbolKind = vim.lsp.protocol.SymbolKind
+      local function h(name)
+        return vim.api.nvim_get_hl(0, { name = name })
+      end
+      local function text_format(symbol)
+        local res = {}
+
+        -- Indicator that shows if there are any other symbols in the same line
+        local stacked_functions_content = symbol.stacked_count > 0 and ("+%s"):format(symbol.stacked_count) or ""
+
+        if symbol.references then
+          local usage = symbol.references <= 1 and "usage" or "usages"
+          local num = symbol.references == 0 and "no" or symbol.references
+          table.insert(res, { "󰌹 ", "SymbolUsageRef" })
+          table.insert(res, { ("%s %s"):format(num, usage), "SymbolUsageContent" })
+        end
+
+        if symbol.definition then
+          if #res > 0 then
+            table.insert(res, { " ", "NonText" })
+          end
+          table.insert(res, { "󰳽 ", "SymbolUsageDef" })
+          table.insert(res, { symbol.definition .. " defs", "SymbolUsageContent" })
+        end
+
+        if symbol.implementation then
+          if #res > 0 then
+            table.insert(res, { " ", "NonText" })
+          end
+          table.insert(res, { "󰡱 ", "SymbolUsageImpl" })
+          table.insert(res, { symbol.implementation .. " impls", "SymbolUsageContent" })
+        end
+
+        if stacked_functions_content ~= "" then
+          if #res > 0 then
+            table.insert(res, { " ", "NonText" })
+          end
+          table.insert(res, { " ", "SymbolUsageImpl" })
+          table.insert(res, { stacked_functions_content, "SymbolUsageContent" })
+        end
+
+        return res
+      end
+      require("symbol-usage").setup({
+        kinds = {
+          SymbolKind.Function,
+          SymbolKind.Method,
+          SymbolKind.Struct,
+          SymbolKind.Interface,
+          SymbolKind.Class,
         },
+        references = {
+          enabled = true,
+          include_declaration = true,
+        },
+        implementation = {
+          enabled = true,
+        },
+        definition = {
+          enabled = true,
+        },
+        vt_position = "end_of_line",
+        text_format = text_format,
       })
     end
-  },
+  }
+
 
   -- -- symbol-usage.nvim displays symbol usage information in virtual text
   -- {
   --   'Wansmer/symbol-usage.nvim',
   --   event = "BufReadPre",
   --   config = function()
-  --     local function h(name) return vim.api.nvim_get_hl(0, { name = name }) end
   --     local function text_format(symbol)
   --       local res = {}
-  --       local round_start = { '', 'SymbolUsageRounding' }
-  --       local round_end = { '', 'SymbolUsageRounding' }
   --       if symbol.references then
   --         local usage = symbol.references <= 1 and 'usage' or 'usages'
   --         local num = symbol.references == 0 and 'no' or symbol.references
-  --         table.insert(res, round_start)
   --         table.insert(res, { ' ', 'SymbolUsageRef' })
   --         table.insert(res, { ('%s %s'):format(num, usage), 'SymbolUsageContent' })
-  --         table.insert(res, round_end)
   --       end
   --       if symbol.definition then
-  --         if #res > 0 then
-  --           table.insert(res, { ' ', 'NonText' })
-  --         end
-  --         table.insert(res, round_start)
+  --         -- if #res > 0 then
+  --         --   table.insert(res, { ' ', 'NonText' })
+  --         -- end
   --         table.insert(res, { '󰳽 ', 'SymbolUsageDef' })
   --         table.insert(res, { symbol.definition .. ' defs', 'SymbolUsageContent' })
-  --         table.insert(res, round_end)
   --       end
   --       if symbol.implementation then
-  --         if #res > 0 then
-  --           table.insert(res, { ' ', 'NonText' })
-  --         end
-  --         table.insert(res, round_start)
   --         table.insert(res, { '󰡱 ', 'SymbolUsageImpl' })
   --         table.insert(res, { symbol.implementation .. ' impls', 'SymbolUsageContent' })
-  --         table.insert(res, round_end)
   --       end
   --       return res
   --     end
