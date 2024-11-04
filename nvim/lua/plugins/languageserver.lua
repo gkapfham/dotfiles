7b3b7be960837bf6d1acf396d247f0c6a582b97f
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2024-11-04 15:17:36 -0500

    Delete the commented-out version of an old approach for LSP symbol display in nvim/lua/plugins/languageserver.lua.

--- nvim/lua/plugins/languageserver.lua
+++ nvim/lua/plugins/languageserver.lua
@@ -238,33 +238,6 @@ return {
     }
   },
 
-  -- -- lsp-lens.nvim for symbol usage information in virtual text
-  -- {
-  --   "VidocqH/lsp-lens.nvim",
-  --   event = "BufReadPre",
-  --   config = function()
-  --     require 'lsp-lens'.setup({
-  --       enable = true,
-  --       include_declaration = true,
-  --       separator = ", ",
-  --       sections = {
-  --         definition = function(count)
-  --           return "󰐖 Definitions " .. count
-  --         end,
-  --         references = function(count)
-  --           return " References " .. count - 1
-  --         end,
-  --         implements = function(count)
-  --           return " Implements " .. count
-  --         end,
-  --         git_authors = function(latest_author, count)
-  --           return " " .. latest_author .. (count - 1 == 0 and "" or (" + " .. count - 1))
-  --         end,
-  --       },
-  --     })
-  --   end
-  -- },
-
   -- symbol-usage.nvim displays symbol usage information in virtual text
   {
     'Wansmer/symbol-usage.nvim',
