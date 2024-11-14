128622359e61daa90206fd5a6b5ffccaa7ce9bd2
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2024-11-14 11:57:49 -0500

    Change back the quarto icon so that it does not look like a commands icon in nvim/lua/plugins/interface.lua.

--- nvim/lua/plugins/interface.lua
+++ nvim/lua/plugins/interface.lua
@@ -229,7 +229,7 @@ return {
             name = "Toml"
           },
           ["qmd"] = {
-            icon = "󰐴",
+            icon = "󱨇",
             name = "Quarto"
           },
           [""] = {
