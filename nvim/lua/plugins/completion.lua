9f6fc8ced76749b76eaef31b8e4ec326f9161f13
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2024-11-26 14:54:14 -0500

    Delete very old commented-out completion settings in the nvim/lua/plugins/completion.lua file.

--- nvim/lua/plugins/completion.lua
+++ nvim/lua/plugins/completion.lua
@@ -632,29 +632,3 @@ return {
   },
 
 }
-
--- old completion settings:
-
--- ["<Tab>"] = cmp.mapping(function(fallback)
---   if cmp.visible() then
---     cmp.select_next_item()
---   elseif luasnip.expand_or_jumpable() then
---     luasnip.expand_or_jump()
---   elseif has_words_before() then
---     cmp.complete()
---   else
---     fallback()
---   end
--- end, { "i", "s" }),
-
--- ["<C-n>"] = cmp.mapping(function(fallback)
---   if cmp.visible() then
---     cmp.select_next_item()
---   elseif luasnip.expand_or_jumpable() then
---     luasnip.expand_or_jump()
---   elseif has_words_before() then
---     cmp.complete()
---   else
---     fallback()
---   end
--- end, { "i", "s" }),
