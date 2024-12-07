3d5ce3166bbab7417efd85fe63e9f8e21294afd6
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2024-12-07 17:35:13 -0500

    Add trouble to the disabling of the lualine winbar in nvim/lua/plugins/lualine.lua.

--- nvim/lua/plugins/lualine.lua
+++ nvim/lua/plugins/lualine.lua
@@ -269,7 +269,7 @@ return {
           component_separators = { left = '', right = '' },
           section_separators = { left = '', right = '' },
           disabled_filetypes = {
-            winbar = { 'neo-tree', 'Outline', 'aerial', "Avante", "AvanteInput" },
+            winbar = { 'neo-tree', 'Outline', 'aerial', "Avante", "AvanteInput", "trouble" },
           },
           always_divide_middle = true,
           globalstatus = true,
