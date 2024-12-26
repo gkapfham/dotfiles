0c143dc6d3471d2cb492f6117f36db7461125ba4
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2024-12-26 09:23:44 -0500

    Adopt a new approach to polybar launching that uses pkill in the polybar/scripts/polybar-launch.sh.

--- polybar/scripts/polybar-launch.sh
+++ polybar/scripts/polybar-launch.sh
@@ -1,20 +1,33 @@
 #!/usr/bin/env bash
 
-# kill any running instances
-killall -q polybar
+# # kill any running instances
+# killall -q polybar
+#
+# # kill confirmed
+# while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+#
+# # launch polybar for laptop
+# # polybar mybar &
+#
+# # launch polybar for all available monitors reported by xrandr
+# # reference: https://github.com/polybar/polybar/issues/763
+# if type "xrandr"; then
+#   for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+#     MONITOR=$m polybar --reload mybar &
+#   done
+# else
+#   # polybar --reload mybar &
+#   polybar mybar &
+# fi
 
-# kill confirmed
-while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+# Terminate already running bar instances
+pkill polybar
 
-# launch polybar for laptop
-# polybar mybar &
+# Wait until the processes have been shut down
+while pgrep -x polybar >/dev/null; do sleep 1; done
 
-# launch polybar for all available monitors reported by xrandr
-# reference: https://github.com/polybar/polybar/issues/763
-if type "xrandr"; then
-  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
-    MONITOR=$m polybar --reload mybar &
-  done
-else
-  polybar --reload mybar &
-fi
+# Launch bars, note that this will disable the same
+# polybar on all monitors, regardless of their resolution
+for m in $(polybar --list-monitors | cut -d":" -f1); do
+    MONITOR=$m polybar mybar &
+done
