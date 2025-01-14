63a64718f80558f5c41372aa81f75f4015dc1f9d
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2025-01-13 21:13:42 -0500

    Delete the commented-out old plugin for using treesitter for pair matching in nvim/lua/plugins/movement.lua.

--- nvim/lua/plugins/movement.lua
+++ nvim/lua/plugins/movement.lua
@@ -77,37 +77,6 @@ return {
     end,
   },
 
-  -- -- nvim-treesitter-pairs
-  -- -- Pair movement and highlighting
-  -- -- (note could not get vim-matchup to work;
-  -- -- it crashes when using % and gives errors
-  -- -- suggesting a problem with treesitter integration)
-  -- {
-  --   "theHamsta/nvim-treesitter-pairs",
-  --   event = "VeryLazy",
-  --   config = function()
-  --     require 'nvim-treesitter.configs'.setup {
-  --       pairs = {
-  --         enable = true,
-  --         disable = {},
-  --         highlight_pair_events = { "CursorMoved" },
-  --         highlight_self = false,
-  --         goto_right_end = false,
-  --         fallback_cmd_normal = "call matchit#Match_wrapper('',1,'n')",
-  --         keymaps = {
-  --           goto_partner = "<leader>%",
-  --           delete_balanced = "X",
-  --         },
-  --         delete_balanced = {
-  --           only_on_first_char = false,
-  --           fallback_cmd_normal = nil,
-  --           longest_partner = true,
-  --         }
-  --       }
-  --     }
-  --   end,
-  -- },
-
   -- marks.nvim
   {
     "chentoast/marks.nvim",
