f8fec69e824682f882cf161c5c348dc7447d66cb
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2025-05-02 17:46:35 -0400

    Stop using the right panel settings for edgy in the nvim/lua/plugins/interface.lua file.

--- nvim/lua/plugins/interface.lua
+++ nvim/lua/plugins/interface.lua
@@ -167,61 +167,61 @@ return {
         -- ToggleTerm.nvim
         {
           title = "Terminal",
-          ft = "toggleterm",
+          ft = "snacks_terminal",
         },
         -- Standard quickfix window
         { ft = "qf", title = "QuickFix" },
       },
-      -- Configure the right panel
-      right = {
-        -- Neotree filesystem
-        {
-          title = "Filesystem",
-          ft = "neo-tree",
-          filter = function(buf)
-            return vim.b[buf].neo_tree_source == "filesystem"
-          end,
-          size = { height = 0.5 },
-        },
-        -- Outline.nvim symbols
-        {
-          title = "Outline",
-          ft = "Outline",
-          pinned = false,
-          size = { height = 0.30 },
-          open = "OutlineOpen"
-        },
-        -- Neotree buffers
-        {
-          title = "Buffers",
-          ft = "neo-tree",
-          filter = function(buf)
-            return vim.b[buf].neo_tree_source == "buffers"
-          end,
-          size = { height = 0.20 },
-          pinned = false,
-          open = "Neotree position=top buffers",
-        },
-        -- Neotree Git status
-        {
-          title = "Git",
-          ft = "neo-tree",
-          filter = function(buf)
-            return vim.b[buf].neo_tree_source == "git_status"
-          end,
-          size = { height = 0.20 },
-          pinned = false,
-          open = "Neotree position=bottom git_status",
-        },
-        -- Aerial symbols
-        {
-          title = "Aerial",
-          open = "AerialOpen",
-          pinned = true,
-          size = { height = 0.20 },
-          ft = "aerial",
-        },
-      },
+      -- -- Configure the right panel
+      -- right = {
+      --   -- Neotree filesystem
+      --   {
+      --     title = "Filesystem",
+      --     ft = "neo-tree",
+      --     filter = function(buf)
+      --       return vim.b[buf].neo_tree_source == "filesystem"
+      --     end,
+      --     size = { height = 0.5 },
+      --   },
+      --   -- Outline.nvim symbols
+      --   {
+      --     title = "Outline",
+      --     ft = "Outline",
+      --     pinned = false,
+      --     size = { height = 0.30 },
+      --     open = "OutlineOpen"
+      --   },
+      --   -- Neotree buffers
+      --   {
+      --     title = "Buffers",
+      --     ft = "neo-tree",
+      --     filter = function(buf)
+      --       return vim.b[buf].neo_tree_source == "buffers"
+      --     end,
+      --     size = { height = 0.20 },
+      --     pinned = false,
+      --     open = "Neotree position=top buffers",
+      --   },
+      --   -- Neotree Git status
+      --   {
+      --     title = "Git",
+      --     ft = "neo-tree",
+      --     filter = function(buf)
+      --       return vim.b[buf].neo_tree_source == "git_status"
+      --     end,
+      --     size = { height = 0.20 },
+      --     pinned = false,
+      --     open = "Neotree position=bottom git_status",
+      --   },
+      --   -- Aerial symbols
+      --   {
+      --     title = "Aerial",
+      --     open = "AerialOpen",
+      --     pinned = true,
+      --     size = { height = 0.20 },
+      --     ft = "aerial",
+      --   },
+      -- },
     },
   },
 
