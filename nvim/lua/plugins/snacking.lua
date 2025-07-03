90a5c12b7d9c723216fa4bf4c3dfaa602846840f
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2025-07-03 19:22:09 -0400

    Remove a not-needed blank line in the nvim/lua/plugins/snacking.lua file.

--- nvim/lua/plugins/snacking.lua
+++ nvim/lua/plugins/snacking.lua
@@ -177,7 +177,6 @@ return {
         vim.keymap.set('t', '<C-l>', [[<Cmd>wincmd l<CR>]], opts)
         vim.keymap.set('t', '<C-w>', [[<C-\><C-n><C-w>]], opts)
       end
-
       vim.cmd('autocmd! TermOpen term://* lua set_terminal_keymaps()')
       vim.api.nvim_create_autocmd("User", {
         pattern = "VeryLazy",
