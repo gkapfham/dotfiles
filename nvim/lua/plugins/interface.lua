95bc2c020810bbd1f6a888afaca2196b1027c2e0
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2025-07-06 19:06:53 -0400

    Improve the documentation and settings for the noice.nvim plugin in nvim/lua/plugins/interface.lua.

--- nvim/lua/plugins/interface.lua
+++ nvim/lua/plugins/interface.lua
@@ -165,15 +165,20 @@ return {
           override = {
             ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
             ["vim.lsp.util.stylize_markdown"] = true,
-            ["cmp.entry.get_documentation"] = true, -- requires hrsh7th/nvim-cmp
+          -- requires hrsh7th/nvim-cmp
+            ["cmp.entry.get_documentation"] = true,
           },
         },
         presets = {
-          bottom_search = true,     -- use a classic bottom cmdline for search
-          command_palette = true,   -- position the cmdline and popupmenu together
-          long_message_to_split = true, -- long messages will be sent to a split
-          inc_rename = false,       -- enables an input dialog for inc-rename.nvim
-          lsp_doc_border = true,   -- add a border to hover docs and signature help
+          -- disable the bottom search, meaning that the
+          -- search appears at the same location as the
+          -- command palette, keeping eyes in the same location
+          bottom_search = false,
+          -- position the cmdline and popupmenu together
+          command_palette = true,
+          long_message_to_split = true,
+          inc_rename = false,
+          lsp_doc_border = true,
         },
       })
     end
