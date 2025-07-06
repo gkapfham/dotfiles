023a85098e7bd9d3d8bd5af591a1af3f15804459
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2025-07-05 23:08:16 -0400

    Add a starting configuration for noice.nvim to the nvim/lua/plugins/interface.lua file.

--- nvim/lua/plugins/interface.lua
+++ nvim/lua/plugins/interface.lua
@@ -143,6 +143,42 @@ return {
     end,
   },
 
+  -- noice.nvim
+  -- Enhanced command line and message display,
+  -- especially nice for the command-palette that
+  -- is visible when you press ":" and for the
+  -- search interface when you press "/"
+  {
+    "folke/noice.nvim",
+    event = "VeryLazy",
+    opts = {
+      -- add any options here
+    },
+    dependencies = {
+      "MunifTanjim/nui.nvim",
+      "rcarriga/nvim-notify",
+    },
+    config = function()
+      require("noice").setup({
+        lsp = {
+          -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
+          override = {
+            ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+            ["vim.lsp.util.stylize_markdown"] = true,
+            ["cmp.entry.get_documentation"] = true, -- requires hrsh7th/nvim-cmp
+          },
+        },
+        presets = {
+          bottom_search = true,     -- use a classic bottom cmdline for search
+          command_palette = true,   -- position the cmdline and popupmenu together
+          long_message_to_split = true, -- long messages will be sent to a split
+          inc_rename = false,       -- enables an input dialog for inc-rename.nvim
+          lsp_doc_border = true,   -- add a border to hover docs and signature help
+        },
+      })
+    end
+  },
+
   -- edgy.nvim for controlling sidebars:
   -- supports the display of multiple sidebars
   -- in the same consistently sized region
