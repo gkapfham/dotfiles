b5b9800ba3aea67110a59d4ee4396719ce486a39
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2025-11-12 14:17:26 -0500

    Improve the completion source for paths with the nvim-comp plugin to use the cwd correct in nvim/lua/plugins/completion.lua.

--- nvim/lua/plugins/completion.lua
+++ nvim/lua/plugins/completion.lua
@@ -718,7 +718,18 @@ return {
             priority = 10,
             keyword_length = 3
           },
-          { name = 'path',                   max_item_count = 10, priority = 5, keyword_length = 2 },
+          {
+            name = 'path',
+            option = {
+              get_cwd = function()
+                return vim.fn.getcwd()
+              end,
+            },
+            max_item_count = 5,
+            priority = 10,
+            keyword_length = 3
+          },
+          -- { name = 'path',               max_item_count = 10, priority = 1, keyword_length = 3 },
           { name = 'nerdfont',               max_item_count = 10, priority = 1, keyword_length = 3 },
           { name = 'nvim_lsp_signature_help' },
         }, {
