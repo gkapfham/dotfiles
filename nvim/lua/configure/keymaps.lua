cc44807beda6c26a07c46d568ceaf09ebde9ec84
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2025-12-18 18:57:34 -0500

    Add the configuration for using the special yank.lua file in the nvim/lua/configure/keymaps.lua file.

--- nvim/lua/configure/keymaps.lua
+++ nvim/lua/configure/keymaps.lua
@@ -44,3 +44,17 @@ nmap <leader>ss :set spell!<CR>
 ]])
 
 -- }}}
+
+-- Yank with file paths {{{
+
+local yank = require 'configure.yank'
+
+vim.keymap.set('v', '<Space>yaa', function()
+  yank.yank_visual_with_path(yank.get_buffer_absolute(), 'absolute')
+end, { desc = '[Y]ank selection with [A]bsolute path' })
+
+vim.keymap.set('v', '<Space>yar', function()
+  yank.yank_visual_with_path(yank.get_buffer_cwd_relative(), 'relative')
+end, { desc = '[Y]ank selection with [R]elative path' })
+
+-- }}}
