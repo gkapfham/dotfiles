3e6d5bdd3a78782903e5da6bac8dc4ac6c9838f4
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2025-12-18 19:14:15 -0500

    Add many more configuration settings and keymaps to the nvim/lua/plugins/versioncontrol.lua file.

--- nvim/lua/plugins/versioncontrol.lua
+++ nvim/lua/plugins/versioncontrol.lua
@@ -2,6 +2,42 @@
 -- Purpose: load and configure all plugins that
 -- integrate with the Git version control system
 
+local is_git_ignored = function(filepath)
+  vim.fn.system('git check-ignore -q ' .. vim.fn.shellescape(filepath))
+  return vim.v.shell_error == 0
+end
+
+local update_left_pane = function()
+  pcall(function()
+    local lib = require 'diffview.lib'
+    local view = lib.get_current_view()
+    if view then
+      -- This updates the left panel with all the files
+      view:update_files()
+    end
+  end)
+end
+
+-- Register handler for file changes in watched directory
+require('configure.directorywatcher').registerOnChangeHandler('diffview', function(filepath, events)
+  local is_in_dot_git_dir = filepath:match '/%.git/' or filepath:match '^%.git/'
+
+  if is_in_dot_git_dir or not is_git_ignored(filepath) then
+    update_left_pane()
+  end
+end)
+
+vim.api.nvim_create_autocmd('FocusGained', {
+  callback = update_left_pane,
+})
+
+vim.api.nvim_create_autocmd('User', {
+  pattern = 'DiffviewViewLeave',
+  callback = function()
+    vim.cmd ':DiffviewClose'
+  end,
+})
+
 -- Make it easy to toggle the git status
 -- window provided by vim-fugitive, ensuring
 -- that the sidebar in the edgy does not close
@@ -52,16 +88,28 @@ return {
     },
     config = function()
       require("diffview").setup {
-      use_icons = true,
-      icons = {
-        folder_closed = "",
-        folder_open = "",
-      },
-      view = {
-        default = {
-          layout = "diff2_vertical",
+        use_icons = true,
+        icons = {
+          folder_closed = "",
+          folder_open = "",
         },
-      },
+        watch_index = true,
+        view = {
+          default = {
+            layout = "diff2_vertical",
+          },
+        },
+        keymaps = {
+          view = {
+            { 'n', 'q', '<cmd>DiffviewClose<cr>', { desc = 'Close diffview' } },
+          },
+          file_panel = {
+            { 'n', 'q', '<cmd>DiffviewClose<cr>', { desc = 'Close diffview' } },
+          },
+          file_history_panel = {
+            { 'n', 'q', '<cmd>DiffviewClose<cr>', { desc = 'Close diffview' } },
+          },
+        }
       }
     end,
   },
