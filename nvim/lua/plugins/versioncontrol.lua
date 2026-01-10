1029177f2b6971d276e58dc201b1c4e2dcfa3618
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2026-01-09 19:31:21 -0500

    Change the staged signs in the nvim/lua/plugins/versioncontrol.lua file.

--- nvim/lua/plugins/versioncontrol.lua
+++ nvim/lua/plugins/versioncontrol.lua
@@ -289,6 +289,14 @@ return {
           changedelete = { text = "~" },
           untracked = { text = "?" },
         },
+        signs_staged = {
+          add = { text = "󰐖" },
+          change = { text = "󰦓" },
+          delete = { text = "󰍵" },
+          topdelete = { text = "" },
+          changedelete = { text = "~" },
+          untracked = { text = "" },
+        },
         numhl = false,
         linehl = false,
         watch_gitdir = {
