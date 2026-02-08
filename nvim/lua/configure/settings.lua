8100b8394cf8c538ee4c07721ff710a1904cee23
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2026-02-08 17:39:17 -0500

    Modify both of the configurations for the guicursor to return to using the default setting in the nvim/lua/configure/settings.lua file.

--- nvim/lua/configure/settings.lua
+++ nvim/lua/configure/settings.lua
@@ -28,10 +28,8 @@ vim.opt.lazyredraw = false
 vim.cmd([[
   set nocursorcolumn
   set nocursorline
-  set guicursor=
-  " set guicursor=n-v-c:block,i-ci-ve:ver25,r-cr:hor20,o:hor50
-		"   \,a:blinkwait700-blinkoff400-blinkon250-Cursor/lCursor
-		"   \,sm:block-blinkwait175-blinkoff150-blinkon175
+  " set guicursor=n-v-c:block,i:hor50
+  " set guicursor=n-v-c-sm:block,i-ci-ve:ver50-Cursor,r-cr-o:hor20
   set ttyfast
 ]])
 
