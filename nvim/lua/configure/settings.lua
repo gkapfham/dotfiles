b44070889fc569eaeb8e287f9472c47051c08bcf
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2026-02-08 17:09:27 -0500

    Disable the fancy configuration for the cursor but add a commented-out option in the nvim/lua/configure/settings.lua file.

--- nvim/lua/configure/settings.lua
+++ nvim/lua/configure/settings.lua
@@ -27,8 +27,11 @@ vim.opt.lazyredraw = false
 -- Display screen redraws faster
 vim.cmd([[
   set nocursorcolumn
-  " set nocursorline
-  set cursorline
+  set nocursorline
+  set guicursor=
+  " set guicursor=n-v-c:block,i-ci-ve:ver25,r-cr:hor20,o:hor50
+		"   \,a:blinkwait700-blinkoff400-blinkon250-Cursor/lCursor
+		"   \,sm:block-blinkwait175-blinkoff150-blinkon175
   set ttyfast
 ]])
 
