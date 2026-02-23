36a542721f06125bf6bc3d027ec9020b7e62df56
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2026-02-23 10:30:47 -0500

    chore: Add the TightWrap commands to the nvim/lua/configure/commands.lua file.

--- nvim/lua/configure/commands.lua
+++ nvim/lua/configure/commands.lua
@@ -4,4 +4,5 @@
 -- Wrapping
 vim.api.nvim_create_user_command("NoWrap", "set textwidth=0", {})
 vim.api.nvim_create_user_command("StandardWrap", "set textwidth=80", {})
+vim.api.nvim_create_user_command("TightWrap", "set textwidth=60", {})
 vim.api.nvim_create_user_command("Wrap", "set textwidth=120", {})
