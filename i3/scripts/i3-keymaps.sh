cb7c0580dc1569019b486a2c4bd57bac7c414dc7
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2026-04-23 15:34:10 -0400

    refactor: Set the correct icon in the i3/scripts/i3-keymaps.sh file.

--- i3/scripts/i3-keymaps.sh
+++ i3/scripts/i3-keymaps.sh
@@ -6,4 +6,4 @@ mod_key=$(sed -nre 's/^set \$mod (.*)/\1/p' ${I3_CONFIG})
 grep "^bindsym" ${I3_CONFIG} \
     | sed "s/-\(-\w\+\)\+//g;s/\$mod/${mod_key}/g;s/Mod1/Alt/g;s/exec //;s/bindsym //;s/^\s\+//;s/^\([^ ]\+\) \(.\+\)$/\2: \1/;s/^\s\+//" \
     | tr -s ' ' \
-    | rofi -width 75 -dmenu -i -p Reminder:
+    | rofi -width 75 -dmenu -i -p ' '
