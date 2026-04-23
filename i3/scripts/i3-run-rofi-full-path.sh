f6bafa2405512ddc933f400ef496922f5cf1a992
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2026-04-23 08:06:37 -0400

    refactor: Define the custom icons and then change the way follow-on script called in the i3/scripts/i3-run-rofi-full-path.sh script.

--- i3/scripts/i3-run-rofi-full-path.sh
+++ i3/scripts/i3-run-rofi-full-path.sh
@@ -9,11 +9,27 @@ fi
 
 # Path to the custom run-icons modi script
 ROFI_RUN_SCRIPT="$HOME/.config/i3/scripts/i3-rofi-run-icons.sh"
+ROFI_RUN_ICON=""
+ROFI_DRUN_ICON=""
+ROFI_COMBI_MODES="${1:-run-icons,window,drun}"
+export ROFI_RUN_ICON
 
 # Use the custom nerd-font icon modi if available;
 # otherwise fall back to the native run mode unchanged
 if [ -x "$ROFI_RUN_SCRIPT" ]; then
-    rofi -show combi -modes "combi,run-icons:$ROFI_RUN_SCRIPT" -combi-modes 'run-icons,window,drun'
+    rofi \
+        -show combi \
+        -modes "combi,run-icons:$ROFI_RUN_SCRIPT" \
+        -combi-modes "$ROFI_COMBI_MODES" \
+        -display-run "$ROFI_RUN_ICON" \
+        -display-drun "$ROFI_DRUN_ICON" \
+        -drun-display-format "$ROFI_DRUN_ICON  {name}"
 else
-    rofi -show combi -modes combi -combi-modes 'run,window,drun'
+    rofi \
+        -show combi \
+        -modes combi \
+        -combi-modes "${ROFI_COMBI_MODES/run-icons/run}" \
+        -display-run "$ROFI_RUN_ICON" \
+        -display-drun "$ROFI_DRUN_ICON" \
+        -drun-display-format "$ROFI_DRUN_ICON  {name}"
 fi
