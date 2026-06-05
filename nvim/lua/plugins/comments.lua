761c8842509bf50eb22b1156694d33262ca2246f
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2026-06-05 17:04:14 -0400

    refactor: Start using the mini.comment plugin instead of Comments.nvim in nvim/lua/plugins/comments.lua.

--- nvim/lua/plugins/comments.lua
+++ nvim/lua/plugins/comments.lua
@@ -4,15 +4,14 @@
 
 return {
 
-  -- Comment.nvim
+  -- mini.comment
   -- Comment source code and other
   -- files in an automatic way
   {
-    "numToStr/Comment.nvim",
+    "echasnovski/mini.comment",
     event = "VeryLazy",
     config = function()
-      local comment = require("Comment")
-      comment.setup()
+      require("mini.comment").setup({})
     end,
   },
 }
